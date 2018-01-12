63a0f36e9d563a0b2b24a69ea0eee26aaa3defc8
Sebastian Bocquet <sebastian.bocquet@gmail.com>
2018-01-12 12:35:41 +0100

    Finalize compute HMF (for now)

--- compute_HMF/compute_HMF.py
+++ compute_HMF/compute_HMF.py
@@ -1,43 +1,114 @@
 from __future__ import division
 import numpy as np
 from scipy.interpolate import interp1d
+import scipy.integrate
+
+DIST_H = 2997.92458
+RHOCRIT = 2.775362e11
 
 class HMFCalculator:
     def __init__(self, Deltacrit):
+        """Initialize Tinker interpolation functions and critical overdensity,
+        and store in self."""
         self.Deltacrit = Deltacrit
-
-        # Initialize Tinker interpolation
+        # Initialize Tinker interpolation (A, a, b, c)
         x = np.log((200., 300., 400., 600., 800., 1200., 1600., 2400., 3200.))
-        # A
         y = (1.858659e-01, 1.995973e-01, 2.115659e-01, 2.184113e-01, 2.480968e-01, 2.546053e-01, 2.600000e-01, 2.600000e-01, 2.600000e-01)
-        self.f1 = interp1d(x, y, kind='cubic')
-        # a
+        self.interp_A = interp1d(x, y, kind='cubic')
         y = (1.466904e+00, 1.521782e+00, 1.559186e+00, 1.614585e+00, 1.869936e+00, 2.128056e+00, 2.301275e+00, 2.529241e+00, 2.661983e+00)
-        self.f2 = interp1d(x, y, kind='cubic')
-        # b
+        self.interp_a = interp1d(x, y, kind='cubic')
         y = (2.571104e+00, 2.254217e+00, 2.048674e+00, 1.869559e+00, 1.588649e+00, 1.507134e+00, 1.464374e+00, 1.436827e+00, 1.405210e+00)
-        self.f3 = interp1d(x, y, kind='cubic')
-        # c
+        self.interp_b = interp1d(x, y, kind='cubic')
         y = (1.193958e+00, 1.270316e+00, 1.335191e+00, 1.446266e+00, 1.581345e+00, 1.795050e+00, 1.965613e+00, 2.237466e+00, 2.439729e+00)
-        self.f4 = interp1d(x, y, kind='cubic')
+        self.interp_c = interp1d(x, y, kind='cubic')
 
 
-    def compute_HMF(self):
-        return 1
+    def compute_HMF(self, block):
+        """Compute Tinker HMF, apply redshift volume, and add to block."""
+        ##### Setup
+        # Data arrays
+        M_arr = np.logspace(13, 16, 301)
+        z_arr = block.get_double_array_1d('matter_power_lin', 'z')
+        k_arr = block.get_double_array_1d('matter_power_lin', 'k_h')
+        Pk = block.get_double_array_nd('matter_power_lin', 'p_k')
+        # cosmological parameters
+        self.Omega_m = block.get_double('cosmological_parameters', 'Omega_m')
+        self.Omega_l = block.get_double('cosmological_parameters', 'omega_lambda')
+        self.w0 = block.get_double('cosmological_parameters', 'w')
+        rho_m = self.Omega_m * RHOCRIT
+        # Mean overdensity at each redshift
+        Deltamean = self.Deltacrit / self.Omega_m_z(z_arr)
 
+        ##### Compute sigma(M)
+        # Radius [M_arr]
+        R = (3 * M_arr / (4 * np.pi * rho_m))**(1/3)
+        # [M_arr, k_arr]
+        kR = k_arr[None,:] * R[:,None]
+        # Window functions [M_arr, k_arr]
+        window = 3 * (np.sin(kR)/kR**3 - np.cos(kR)/kR**2)
+        dwindow = 3/kR**4 * (3*kR*np.cos(kR) + ((kR**2 - 3)*np.sin(kR)))
+        # Integrands [z_arr, M_arr, k_arr]
+        integrand_sigma2 = Pk[:,None,:] * window[None,:,:]**2 * k_arr[None,None,:]**2
+        integrand_dsigma2dM = Pk[:,None,:] * window[None,:,:] * dwindow[None,:,:] * k_arr[None,None,:]**3
+        # Sigma^2 and dsigma^2/dM [z_arr, M_arr]
+        sigma2 = .5/np.pi**2 * np.trapz(integrand_sigma2, k_arr, axis=-1)
+        dsigma2dM = np.pi**-2 * R[None,:]/M_arr[None,:]/3 * np.trapz(integrand_dsigma2dM, k_arr, axis=-1)
 
+        ##### Compute Tinker HMF (unit volume)
+        A, a, b, c = np.array([self.Tinker_params(z_arr[i], Deltamean[i]) for i in range(len(z_arr))]).T
+        # HMF [z_arr, M_arr]
+        fsigma = A[:,None] * ((sigma2**.5/b[:,None])**-a[:,None] + 1) * np.exp(-c[:,None]/sigma2)
+        dNdlnM_noVol = - fsigma * rho_m * dsigma2dM/2/sigma2
+
+        ##### Apply redshift volume
+        deltaV = self.deltaV(z_arr[1:])
+        deltaV = np.insert(deltaV, 0, deltaV[1])
+        dNdlnM = dNdlnM_noVol * deltaV[:,None]
+
+        ##### Store HMF
+        block.put_double_array_1d('HMF', 'M_arr', M_arr)
+        block.put_double_array_1d('HMF', 'z_arr', z_arr)
+        block.put_double_array_nd('HMF', 'dNdlnM_unitVol', dNdlnM_noVol)
+        block.put_double_array_nd('HMF', 'dNdlnM', dNdlnM)
 
 
     def Tinker_params(self, z, Deltamean):
-        if Deltamean>3200:
-            z0params = [.26, 2.66, 1.41, 2.44]
+        """For given redshift and mean overdensity, return list of four Tinker
+        parameters. If outside defined overdensity, return last valid number."""
+        # Parameters at z=0
+        if Deltamean<200:
+            A, a, b, c = [.186, 1.47, 2.57, 1.19]
+        elif Deltamean>3200:
+            A, a, b, c = [.26, 2.66, 1.41, 2.44]
         else:
-            logDeltamean = np.log(Deltamean)
-            z0params = [self.f1(logDeltamean), self.f2(logDeltamean), self.f3(logDeltamean), self.f4(logDeltamean)]
+            lnDeltamean = np.log(Deltamean)
+            A, a, b, c = [self.interp_A(lnDeltamean), self.interp_a(lnDeltamean), self.interp_b(lnDeltamean), self.interp_c(lnDeltamean)]
+        # Redshift evolution
         logalpha = -(.75/np.log10(Deltamean/75.))**1.2
-        alpha = 10.**logalpha
+        alpha = 10**logalpha
+        A*= (1+z)**-.14
+        a*= (1+z)**-.06
+        b*= (1+z)**-alpha
+        return A, a, b, c
 
-        self.A = z0params[0] * (1.+z)**-.14
-        self.a = z0params[1] * (1.+z)**-.06
-        self.b = z0params[2] * (1.+z)**-alpha
-        self.c = z0params[3] * 1.
+
+    ##########
+    # Some cosmology function.
+    # This should be re-organized centrally at some point!
+    def Ez(self, z):
+        """Return the growth factor."""
+        return (self.Omega_m*(1+z)**3 + self.Omega_l*(1+z)**(3*(1+self.w0)))**.5
+
+    def Omega_m_z(self, z):
+        """Return Omega_m(z)."""
+        return self.Omega_m * (1+z)**3 / self.Ez(z)**2
+
+    def AngDiamDist(self, z):
+        """Return angular diameter distance in Mpc/h."""
+        integrand = lambda z_int: 1/self.Ez(z_int)
+        return scipy.integrate.quad(integrand, 0., z)[0] * DIST_H/(1+z)
+
+    def deltaV(self, z_arr):
+        """Return solid angle volume as a function of redshift [(Mpc/h)^3]."""
+        AngDiamDist_arr = [self.AngDiamDist(z) for z in z_arr]
+        return DIST_H * ((1+z_arr) * AngDiamDist_arr)**2 / self.Ez(z_arr)
--- compute_HMF/compute_HMF_interface.py
+++ compute_HMF/compute_HMF_interface.py
@@ -1,20 +1,15 @@
 from __future__ import division
 import numpy as np
-from cosmosis.datablock import names, option_section
+from cosmosis.datablock import option_section
 import compute_HMF
-
-cosmo = names.cosmological_parameters
 
 def setup(options):
     Deltacrit = options.get_double(option_section, 'Deltacrit', default=500.)
-    # Initialize HMF calculator
     HMF_calculator = compute_HMF.HMFCalculator(Deltacrit)
     return HMF_calculator
 
 def execute(block, HMF_calculator):
-    # print "block.sections", block.sections()
-    block.put('hmf', 'dNdlnM', 12.3)
-
+    HMF_calculator.compute_HMF(block)
     return 0
 
 def cleanup(config):
