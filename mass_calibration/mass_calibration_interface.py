62fc22ac6b34dc0efc902bbe76e286bc331bc317
Sebastian Bocquet <sebastian.bocquet@gmail.com>
2018-01-12 12:36:18 +0100

    clean up interfaces

--- mass_calibration/mass_calibration_interface.py
+++ mass_calibration/mass_calibration_interface.py
@@ -1,19 +1,14 @@
 from __future__ import division
 import numpy as np
-from cosmosis.datablock import names, option_section
 import masscalib
 
-cosmo = names.cosmological_parameters
-likes = names.likelihoods
-
 def setup(options):
-    # Initialize number count
     mass_calibration = masscalib.MassCalibration(options)
     return mass_calibration
 
 def execute(block, mass_calibration):
-    block[likes, 'MASS_CALIBRATION_LIKE'] = mass_calibration.lnlike(block)
-
+    lnlike = mass_calibration.lnlike(block)
+    block.put_double('likelihoods', 'MASS_CALIBRATION_LIKE', lnlike)
     return 0
 
 def cleanup(config):
