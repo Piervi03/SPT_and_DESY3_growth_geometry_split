d9b20fca99fc2e3221d783d9a4e8cac62da53b2b
Sebastian Bocquet <sebastian.bocquet@gmail.com>
2018-01-18 15:09:04 +0100

    Small bugfix to allow for reading in precalculated HMF

--- compute_HMF_interface.py
+++ compute_HMF_interface.py
@@ -4,13 +4,17 @@ from cosmosis.datablock import option_section
 import compute_HMF
 import pickle
 
+class EmptyClass:
+    pass
+
 def setup(options):
     recalc_HMF = options.get_bool(option_section, 'recalc_HMF', default=True)
     save_HMF_to_disk = options.get_bool(option_section, 'save_HMF_to_disk', default=False)
     if recalc_HMF:
         HMF_calculator = compute_HMF.HMFCalculator(options)
     else:
-        HMF_calculator = pickle.load(open('HMF.pkl', 'rb'))
+        HMF_calculator = EmptyClass()
+        HMF_calculator.HMF = pickle.load(open('HMF.pkl', 'rb'))
     HMF_calculator.recalc_HMF = recalc_HMF
     HMF_calculator.save_HMF_to_disk = save_HMF_to_disk
     return HMF_calculator
@@ -24,9 +28,9 @@ def execute(block, HMF_calculator):
                 'dNdlnM': block.get_double_array_nd('HMF', 'dNdlnM')}
             pickle.dump(HMF, open('HMF.pkl', 'wb'))
     else:
-        block.put_double_array_1d('HMF', 'M_arr', HMF_calculator['M_arr'])
-        block.put_double_array_1d('HMF', 'z_arr', HMF_calculator['z_arr'])
-        block.put_double_array_nd('HMF', 'dNdlnM', HMF_calculator['dNdlnM'])
+        block.put_double_array_1d('HMF', 'M_arr', HMF_calculator.HMF['M_arr'])
+        block.put_double_array_1d('HMF', 'z_arr', HMF_calculator.HMF['z_arr'])
+        block.put_double_array_nd('HMF', 'dNdlnM', HMF_calculator.HMF['dNdlnM'])
     return 0
 
 def cleanup(config):
