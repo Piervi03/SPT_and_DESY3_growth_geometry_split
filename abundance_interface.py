6baf5e59b44ef6314641bbca5b76b83a84337147
Sebastian Bocquet <sebastian.bocquet@gmail.com>
2018-01-24 17:07:09 +0100

    cosmosis has its own timing option...

--- abundance_interface.py
+++ abundance_interface.py
@@ -2,19 +2,12 @@ from __future__ import division
 import numpy as np
 import abundance
 
-if __debug__:
-    import time
-
 def setup(options):
     number_count = abundance.NumberCount(options)
     return number_count
 
 def execute(block, number_count):
-    if __debug__:
-        t0 = time.time()
     lnlike = float(number_count.lnlike(block))
-    if __debug__:
-        print "Abundance took", time.time()-t0
     block.put_double('likelihoods', 'ABUNDANCE_LIKE', lnlike)
     return 0
 
