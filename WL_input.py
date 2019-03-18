ea6bf51d6ebed3bbb45feff0f9f4d23be3379c69
Sebastian Bocquet <sebastian.bocquet@gmail.com>
2019-03-18 14:26:50 +0100

    new WL mock generator

---
+++ WL_input.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+r_deg = np.linspace(.05,.15,11)
+
+# DES Y3
+source_p_arcmin2 = 6
+
+# Completely arbitrary
+source_lognorm_dist_mean = .7
+source_lognorm_dist_sigma = .5
+
+# From Grandis+19
+shape_noise = .27
+
+# Offset in redshift behind the cluster
+z_offset = .1
