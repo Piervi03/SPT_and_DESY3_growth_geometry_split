correct scaling with h for Yx and Mgas

scaling_relations.py
@@ -23,16 +23,16 @@ def mass2obs(name, mass, z, scaling, cosmology):
         return np.exp(lnzeta)
     elif name=='Yx':
         if scaling['YXPARAM']=='SPT_XVP':
-            return 3*(mass*1e-14/(scaling['Ax'] * cosmology['h']**1.5
-                * (cosmology['h']/.72)**(2.5*scaling['Bx']-1.5)
-                * cosmo.Ez(z, cosmology)**scaling['Cx']))**(1/scaling['Bx'])
-        elif scaling['YXPARAM']=='Munich':
-            return scaling['Ax']* (mass/cosmology['h']**1.5/scaling['XraymPivot'])**scaling['Bx']\
+            return 3 * (self.cosmology['h']/.7)**-2.5 * (mass/1e14 /.7**(3/2) / scaling['Ax'] \
+                / cosmo.Ez(z, cosmology)**scaling['Cx']))**(1/scaling['Bx'])
+        elif scaling['YXPARAM']=='obs-mass':
+            return scaling['Ax'] * (self.cosmology['h']/.7)**-2.5 * (mass/cosmology['h']/scaling['XraymPivot'])**scaling['Bx'] \
                 * (cosmo.Ez(z, cosmology)/cosmo.Ez(.6, cosmology))**scaling['Cx']
     elif name=='Mgas':
-        lnMgas = np.log(scaling['XraymPivot'] * scaling['Ax']) + scaling['Bx']*np.log(mass/scaling['XraymPivot']/cosmology['h'])\
-            + scaling['Cx']*np.log(cosmo.Ez(z, cosmology)/cosmo.Ez(.6, cosmology))\
-            + scaling['Ex']*np.log(mass/scaling['XraymPivot']/cosmology['h'])*np.log(cosmo.Ez(z, cosmology)/cosmo.Ez(.6, cosmology))
+        lnMgas = np.log(scaling['XraymPivot'] * scaling['Ax']) - 2.5 * np.log(cosmology['h']/.7) \
+            + scaling['Bx'] * np.log(mass/scaling['XraymPivot']/cosmology['h'])\
+            + scaling['Cx'] * np.log(cosmo.Ez(z, cosmology)/cosmo.Ez(.6, cosmology))\
+            + scaling['Ex'] * np.log(mass/scaling['XraymPivot']/cosmology['h'])*np.log(cosmo.Ez(z, cosmology)/cosmo.Ez(.6, cosmology))
         return np.exp(lnMgas)
     elif name=='disp':
         h70z = cosmology['h']/.7*cosmo.Ez(z, cosmology)
@@ -62,7 +62,7 @@ def dlnM_dlnobs(name, scaling, cosmology=None, M0_arr=None, z=None):
     elif name=='Yx':
         if scaling['YXPARAM']=='SPT_XVP':
             return 1/(1/scaling['Bx'] - scaling['dlnMg_dlnr']/3)
-        elif scaling['YXPARAM']=='Munich':
+        elif scaling['YXPARAM']=='obs-mass':
             return 1/(scaling['Bx'] - scaling['dlnMg_dlnr']/3)
     elif name=='Mgas':
         return 1/(scaling['Bx'] - scaling['dlnMg_dlnr']/3)