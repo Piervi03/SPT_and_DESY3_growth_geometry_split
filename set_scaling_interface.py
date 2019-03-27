d8fc6bc7e22b53d5994cbb3f6ca6fe031aaec381
Sebastian Bocquet <sebastian.bocquet@gmail.com>
2019-03-27 20:43:01 +0100

    clean interface for set_scaling

--- set_scaling_interface.py
+++ set_scaling_interface.py
@@ -1,12 +1,37 @@
 from __future__ import division
+import imp
+
+from cosmosis.datablock import option_section
+
 import set_scaling
 
 def setup(options):
-    scaling = set_scaling.SetScaling(options)
-    return scaling
+    scaling_setter = set_scaling.SetScaling()
+    # WL simulation calibration data
+    WLsimcalibfile = options.get_string(option_section, 'WLsimcalibfile')
+    WLsimcalib = imp.load_source('WLsimcalib', WLsimcalibfile)
+    scaling_setter.WLcalib = scaling_setter.WLcalibration
+    return scaling_setter
 
-def execute(block, scaling):
-    if scaling.execute(block):
+def execute(block, scaling_setter):
+    # Read scaling relation parameters from block
+    scaling = {}
+    for p in ['Dsz', 'Dx', 'Drichness', 'WLbias', 'WLscatter']:
+        scaling[p] = block.get_double('mor_parameters', p)
+    for p in ['MegacamBias', 'DESbias', 'HSTbias']:
+        scaling[p] = block.get_double('mor_parameters', p)
+    for p in ['rhoSZWL', 'rhoSZX', 'rhoWLX', 'rhoSZrichness', 'rhoXdisp', 'rhoSZdisp']:
+        scaling[p] = block.get_double('mor_parameters', p)
+    # Set everything
+    if scaling_setter.execute(scaling):
+        # Put into block
+        for p in ['bWL_Megacam', 'DWL_Megacam', 'bWL_DES', 'DWL_DES']:
+            block.put_double('mor_parameters', p, scaling[p])
+        for p in ['cov_X_SZ', 'cov_richness_SZ', 'cov_Megacam_SZ', 'cov_DES_SZ', 'cov_Megacam_X_SZ', 'cov_DES_X_SZ']:
+            block.put_double_array_nd('mor_parameters', p, scaling[p])
+        for name in scaling_setter.WLcalib['HSTsim'].keys():
+            block.put_double_array_nd('mor_parameters', 'cov_HST_SZ_%s'%name, scaling['cov_HST_SZ_%s'%name])
+            block.put_double_array_nd('mor_parameters', 'cov_HST_X_SZ_%s'%name, scaling['cov_HST_X_SZ_%s'%name])
         return 0
     else:
         return 1
