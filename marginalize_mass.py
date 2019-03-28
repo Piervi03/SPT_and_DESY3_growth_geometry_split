7ccad1c36a1031891ddf84ca6e4e2a762011cd53
Sebastian Bocquet <sebastian.bocquet@gmail.com>
2019-03-28 11:41:53 +0100

    Clean interface for marginalize_mass

--- marginalize_mass.py
+++ marginalize_mass.py
@@ -4,52 +4,25 @@ import os
 from scipy.interpolate import RectBivariateSpline
 from astropy.table import Table
 
-from cosmosis.datablock import option_section
 import cosmo, Mconversion_concentration
 
 ################################################################################
 class MarginalizeMass:
-    def __init__(self, options):
-        ##### Global variables
-        self.SZmPivot = options.get_double(option_section, 'SZmPivot')
-        # SPT survey
-        SPT_survey_fields = options.get_string(option_section, 'SPT_survey_fields')
-        assert os.path.isfile(SPT_survey_fields), "SPT survey table does not exist"
-        self.SPT_survey = Table.read(SPT_survey_fields, format='ascii.commented_header')
-        # Cluster catalog
-        SPTcatalogfile = options.get_string(option_section, 'SPTcatalogfile')
-        assert os.path.isfile(SPTcatalogfile), "SPT catalog file does not exist"
-        self.catalog = Table.read(SPTcatalogfile)
-        ##### M-c relation for M200
+    def __init__(self):
+        # M-c relation for M200
         self.MCrel = Mconversion_concentration.ConcentrationConversion('Duffy08')
 
 
     ##########
-    def do_it(self, block):
+    def do_it(self):
         """Return ln-likelihood for SPT cluster abundance."""
-        # Only need cosmo for E(z)-type stuff
-        self.cosmology = {'Omega_m': block.get_double('cosmological_parameters', 'Omega_m'),
-            'Omega_l': block.get_double('cosmological_parameters', 'omega_lambda'),
-            'w0': block.get_double('cosmological_parameters', 'w'),
-            'wa': block.get_double('cosmological_parameters', 'wa')}
-        # SZ scaling relation parameters
-        self.Asz = block.get_double('mor_parameters', 'Asz')
-        self.Bsz = block.get_double('mor_parameters', 'Bsz')
-        self.Csz = block.get_double('mor_parameters', 'Csz')
-        self.Dsz = block.get_double('mor_parameters', 'Dsz')
-        # Halo mass function
-        self.HMF = {'M_arr': block.get_double_array_1d('HMF', 'M_arr'),
-            'z_arr': block.get_double_array_1d('HMF', 'z_arr'),
-            'dNdlnM': block.get_double_array_nd('HMF', 'dNdlnM')}
-        self.HMF['len_z'] = len(self.HMF['z_arr'])
-
         ##### Set up interpolation for HMF
         HMF_in = self.HMF['dNdlnM'][1:,:]
         if np.any(HMF_in==0):
             HMF_in[np.where(HMF_in==0)] = np.nextafter(0, 1)
         HMF_interp = RectBivariateSpline(np.log(self.HMF['z_arr'][1:]), np.log(self.HMF['M_arr']), np.log(HMF_in))
 
-
+        ##### Now go through cluster catalog
         M500, M200, weight = [], [], []
         for i,name in enumerate(self.catalog['SPT_ID']):
             if self.catalog['REDSHIFT_LIMIT'][i]>0:
@@ -60,8 +33,6 @@ class MarginalizeMass:
             # Normalized HMF
             lnHMF_z_ = HMF_interp(np.log(self.catalog['REDSHIFT'][i]), np.log(self.HMF['M_arr']))[0]
             Ntot_ = np.sum(np.diff(self.HMF['M_arr']) * np.exp(.5*(lnHMF_z_[1:]+lnHMF_z_[:-1])))
-            #HMF_z_ = np.exp(HMF_interp(np.log(self.catalog['REDSHIFT'][i]), np.log(self.HMF['M_arr'])))[0]
-            #HMF_z_/= np.sum(np.diff(self.HMF['M_arr']) * np.exp(.5*np.log(HMF_z_[1:]*HMF_z_[:-1])))
 
             xi = 0
             while xi<=2.65:
@@ -77,22 +48,18 @@ class MarginalizeMass:
             M200_ = self.MCrel.MDelta_to_M200(M500_, 500., self.catalog['REDSHIFT'][i])
             # P(M,z) a.k.a. the halo mass function
             weight_ = np.exp(HMF_interp(np.log(self.catalog['REDSHIFT'][i]), np.log(M500_)))[0,0] / Ntot_
-            #weight_ = np.exp(np.interp(np.log(M500_), np.log(self.HMF['M_arr']), np.log(HMF_z_)))
 
             M500.append(M500_)
             M200.append(M200_)
             weight.append(weight_)
 
-        for i in range(len(M500)):
-            block.put_double('marge_mass', 'M500_%d'%i, M500[i])
-            block.put_double('marge_mass', 'weight_%d'%i, weight[i])
-            block.put_double('marge_mass', 'M200_%d'%i, M200[i])
+        output_arr = np.vstack((M500, M200, weight))
 
+        return output_arr
 
 
 
     ########## Utility functions
-
     def xi2zeta(self, xi):
         if xi>2.65:
             return (xi**2 - 3)**.5
--- marginalize_mass_interface.py
+++ marginalize_mass_interface.py
@@ -1,13 +1,49 @@
 from __future__ import division
 import numpy as np
+from cosmosis.datablock import option_section
 import marginalize_mass
 
 def setup(options):
-    marge_mass = marginalize_mass.MarginalizeMass(options)
+    marge_mass = marginalize_mass.MarginalizeMass()
+
+    ##### Global variables
+    marge_mass.SZmPivot = options.get_double(option_section, 'SZmPivot')
+    # SPT survey
+    SPT_survey_fields = options.get_string(option_section, 'SPT_survey_fields')
+    assert os.path.isfile(SPT_survey_fields), "SPT survey table does not exist"
+    marge_mass.SPT_survey = Table.read(SPT_survey_fields, format='ascii.commented_header')
+    # Cluster catalog
+    SPTcatalogfile = options.get_string(option_section, 'SPTcatalogfile')
+    assert os.path.isfile(SPTcatalogfile), "SPT catalog file does not exist"
+    marge_mass.catalog = Table.read(SPTcatalogfile)
+
     return marge_mass
 
 def execute(block, marge_mass):
-    marge_mass.do_it(block)
+    # Only need cosmo for E(z)-type stuff
+    marge_mass.cosmology = {'Omega_m': block.get_double('cosmological_parameters', 'Omega_m'),
+        'Omega_l': block.get_double('cosmological_parameters', 'omega_lambda'),
+        'w0': block.get_double('cosmological_parameters', 'w'),
+        'wa': block.get_double('cosmological_parameters', 'wa')}
+    # SZ scaling relation parameters
+    marge_mass.Asz = block.get_double('mor_parameters', 'Asz')
+    marge_mass.Bsz = block.get_double('mor_parameters', 'Bsz')
+    marge_mass.Csz = block.get_double('mor_parameters', 'Csz')
+    marge_mass.Dsz = block.get_double('mor_parameters', 'Dsz')
+    # Halo mass function
+    marge_mass.HMF = {'M_arr': block.get_double_array_1d('HMF', 'M_arr'),
+        'z_arr': block.get_double_array_1d('HMF', 'z_arr'),
+        'dNdlnM': block.get_double_array_nd('HMF', 'dNdlnM')}
+    marge_mass.HMF['len_z'] = len(marge_mass.HMF['z_arr'])
+
+    #### Get marginalized mass draws
+    mass_arr = marge_mass.do_it()
+
+    ##### Put back into block
+    for i in range(len(mass_arr)):
+        block.put_double('marge_mass', 'M500_%d'%i, mass_arr[i,0])
+        block.put_double('marge_mass', 'M200_%d'%i, mass_arr[i,1])
+        block.put_double('marge_mass', 'weight_%d'%i, mass_arr[i,2])
 
     return 0
 
