16540588c2293544eba956b6a027b92dc8251888
Sebastian Bocquet <sebastian.bocquet@gmail.com>
2019-05-06 16:30:38 +0200

    WL mock radial range defined in Mpc instead of deg

--- WL_input.py
+++ WL_input.py
@@ -2,7 +2,12 @@ import numpy as np
 
 random_seed = 0
 
-r_deg = np.linspace(.05,.15,11)
+WL_z_max = 1
+
+# Radial range, will be converted in degrees
+r_minmax = .25, 3
+r_edge_Mpc = np.logspace(np.log10(r_minmax[0]),np.log10(r_minmax[1]),11)
+r_bin_Mpc = np.sqrt(r_edge_Mpc[1:]**2 - r_edge_Mpc[:-1]**2)
 
 # DES Y3
 source_p_arcmin2 = 6
