ef432558aef6cf61d46f3f38c70496ca59e07ff2
Sebastian Bocquet <sebastian.bocquet@gmail.com>
2019-08-05 09:55:32 +0200

    Fix bug in radial bin centers

--- WL_input.py
+++ WL_input.py
@@ -8,13 +8,19 @@ WL_z_max = 1
 
 beta_bias_file = '../SPT_cluster_data/sci_uncertainty_zdiff_01_40bins_new.dat'
 
-# Radial range, will be converted in degrees
+# Radial range, will be converted in arcmin
 r_minmax = .25, 3
 r_edge_Mpc = np.linspace(r_minmax[0], r_minmax[1], 11)
-r_bin_Mpc = np.sqrt(r_edge_Mpc[1:]**2 - r_edge_Mpc[:-1]**2)
+r_bin_Mpc = np.array([np.sum(r_edge_Mpc[i:i+2]**3)/np.sum(r_edge_Mpc[i:i+2]**2)
+                      for i in range(len(r_edge_Mpc)-1)])
+
+# Miscentering parameters [arcmin]
+rho0 = .6
+sigma0 = .1
+sigma1 = .2
 
 # DES Y3
-source_p_arcmin2 = 6
+source_p_arcmin2 = 30
 
 # Completely arbitrary
 source_lognorm_dist_mean = .7
