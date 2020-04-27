81e3189f30a47f91be5f3f8efbcabe68c754f97c
Sebastian Bocquet <sebastian.bocquet@gmail.com>
2020-04-27 12:01:35 +0200

    Mira-Titan mass function emulator

---
+++ compute_HMF_MiraTitan.py
@@ -0,0 +1,47 @@
+import numpy as np
+from scipy.interpolate import RectBivariateSpline
+
+import MiraTitanHMFemulator
+import cosmo
+import Mconversion_concentration
+
+class HMFCalculator:
+    def __init__(self, Deltacrit, mcType, z_arr, M_arr):
+        self.HMFemu = MiraTitanHMFemulator.Emulator()
+        self.Deltacrit = Deltacrit
+        self.mcType = mcType
+        self.z_arr = z_arr
+        self.M_arr = M_arr
+
+    def compute_HMF(self, cosmology):
+        if not self.HMFemu.validate_params(cosmology):
+            return 1
+
+        if self.Deltacrit==200:
+            self.dNdlnM_unitVol = self.HMFemu.predict(cosmology, self.z_arr, self.M_arr, get_errors=False)
+
+        else:
+            emu_dict = self.HMFemu.predict_raw_emu(cosmology)
+            HMF_interp_input = np.log(np.nextafter(0,1)) * np.ones((len(self.HMFemu.z_arr_asc), 3501))
+            for i,emu_z in enumerate(self.HMFemu.z_arr_asc):
+                HMF_interp_input[i,:len(emu_dict[emu_z]['HMF'])] = np.log(emu_dict[emu_z]['HMF'])
+            HMF_interp = RectBivariateSpline(self.HMFemu.z_arr_asc, np.linspace(13, 16.5, 3501), HMF_interp_input, kx=1, ky=1)
+
+            MCrel = Mconversion_concentration.ConcentrationConversion(self.mcType, cosmology,
+                                                                      setup_interp=True, interp_massdef=self.Deltacrit)
+            lnM200 = MCrel.lnM_to_lnM200(self.z_arr, np.log(self.M_arr))
+
+            self.dNdlnM_unitVol = np.empty((len(self.z_arr), len(self.M_arr)))
+            for z_id, z in enumerate(self.z_arr):
+                dlnM200_dlnMDelta = (lnM200[z_id,2:]-lnM200[z_id,:-2]) / (np.log(self.M_arr[2:]/self.M_arr[:-2]))
+                dlnM200_dlnMDelta = np.insert(dlnM200_dlnMDelta, 0, (lnM200[z_id,1]-lnM200[z_id,0])/np.log(self.M_arr[1]/self.M_arr[0]))
+                dlnM200_dlnMDelta = np.append(dlnM200_dlnMDelta, (lnM200[z_id,-1]-lnM200[z_id,-2])/np.log(self.M_arr[-1]/self.M_arr[-2]))
+                self.dNdlnM_unitVol[z_id] = np.exp(HMF_interp(z, lnM200[z_id]/np.log(10))) * dlnM200_dlnMDelta
+
+        ##### Apply redshift volume
+        deltaV = cosmo.deltaV(self.z_arr[1:], cosmology)
+        deltaV = np.insert(deltaV, 0, deltaV[1])
+        self.dNdlnM = self.dNdlnM_unitVol * deltaV[:,None]
+
+        ##### Return HMF
+        return 0
---
+++ compute_HMF_MiraTitan_interface.py
@@ -0,0 +1,61 @@
+from __future__ import division
+import numpy as np
+import xarray as xr
+import os
+
+from cosmosis.datablock import option_section
+
+import compute_HMF_MiraTitan
+
+
+class EmptyClass:
+    pass
+
+def setup(options):
+    # Print repository status
+    path_to_repo = os.path.dirname(__file__)
+    os.system("git --git-dir=%s/.git --work-tree=%s status"%(path_to_repo, path_to_repo))
+    os.system("git --git-dir=%s/.git --work-tree=%s "%(path_to_repo, path_to_repo)+"show -s --format=%h")
+    # Proceed with actual setup
+    Deltacrit = options.get_double(option_section, 'Deltacrit', default=500.)
+    mcType = options.get_string(option_section, 'mcType')
+    z = options.get_double_array_1d(option_section, 'z_arr')
+    z_arr = np.linspace(z[0], z[1], int(z[2]))
+    m = options.get_double_array_1d(option_section, 'M_arr')
+    M_arr = np.logspace(m[0], m[1], int(m[2]))
+
+    HMF_calculator = compute_HMF_MiraTitan.HMFCalculator(Deltacrit, mcType, z_arr, M_arr)
+
+    return HMF_calculator
+
+def execute(block, HMF_calculator):
+    # Only need cosmo for E(z)-type stuff
+    cosmology = {
+        'Omega_m': block.get_double('cosmological_parameters', 'Omega_m'),
+        'Omega_b': block.get_double('cosmological_parameters', 'Omega_b'),
+        'Omega_nu': block.get_double('cosmological_parameters', 'Omega_nu'),
+        'Omega_l': block.get_double('cosmological_parameters', 'omega_lambda'),
+        'Ommh2': block.get_double('cosmological_parameters', 'ommh2'),
+        'Ombh2': block.get_double('cosmological_parameters', 'ombh2'),
+        'Omnuh2': block.get_double('cosmological_parameters', 'omnuh2'),
+        'h': block.get_double('cosmological_parameters', 'hubble')/100,
+        'n_s': block.get_double('cosmological_parameters', 'n_s'),
+        'sigma_8': block.get_double('cosmological_parameters', 'sigma_8'),
+        'w0': block.get_double('cosmological_parameters', 'w'),
+        'wa': block.get_double('cosmological_parameters', 'wa'),
+        'w_0': block.get_double('cosmological_parameters', 'w'),
+        'w_a': block.get_double('cosmological_parameters', 'wa'),}
+    # Compute the HMF
+    bad = HMF_calculator.compute_HMF(cosmology)
+    if bad:
+        return 1
+    # Put it into block
+    block.put_double_array_1d('HMF', 'M_arr', HMF_calculator.M_arr)
+    block.put_double_array_1d('HMF', 'z_arr', HMF_calculator.z_arr)
+    block.put_double_array_nd('HMF', 'dNdlnM_unitVol', HMF_calculator.dNdlnM_unitVol)
+    block.put_double_array_nd('HMF', 'dNdlnM', HMF_calculator.dNdlnM)
+
+    return 0
+
+def cleanup(config):
+    pass
