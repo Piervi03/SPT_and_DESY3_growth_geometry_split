f75693266ca630eaa7628d987501ebc46977462a
Sebastian Bocquet <sebastian.bocquet@gmail.com>
2020-11-10 21:10:44 +0100

    HMF_convo: use cython multivar normal; cleaner interface; DES-richness-SZ

--- HMF_convo.py
+++ HMF_convo.py
@@ -1,11 +1,12 @@
 from __future__ import division, print_function
 import numpy as np
 from math import sqrt as msqrt
+import time
 
 from multiprocessing import Pool
 from scipy.interpolate import RectBivariateSpline
-from scipy.stats import multivariate_normal
 
+import multivariate_normal as cy_multivariate_normal
 import convolution, scaling_relations
 
 # Because multiprocessing within classes doesn't really work...
@@ -18,6 +19,12 @@ class MultiObsConvolution:
     def __init__(self, observable_pairs,
                  pairs_zmin, pairs_zmax, pairs_Nz,
                  NPROC):
+        # Sigma-clipping in convolutions
+        self.N_sigma = np.array([4, 3])
+        # Sparsity of returned arrays
+        self.compression = 10
+        # Number of processes (0 for simple loop)
+        self.NPROC = NPROC
 
         self.pairnames_2d = ['Yx_SZ', 'Mgas_SZ', 'Megacam_SZ', 'richness_SZ']
         self.pairnames_2d_DES = ['DES_SZ',]
@@ -35,41 +42,36 @@ class MultiObsConvolution:
                               'DES_Mgas_SZ': ['WLDES', 'Mgas'],
                               'DES_richness_SZ': ['WLDES', 'richness'],
                               }
-        # Sigma-clipping in convolutions
-        self.N_sigma = 4
-        self.scaling = {}
-        self.covmat = {}
-        self.compression = 10
 
-        self.NPROC = NPROC
         self.observable_pairs, self.pairs_zmin, self.pairs_zmax, self.pairs_Nz = [], [], [], []
         for pair, zmin, zmax, Nz in zip(observable_pairs, pairs_zmin, pairs_zmax, pairs_Nz):
-            if (pair in self.pairnames_2d) | (pair in self.pairnames_3d) | (pair in self.pairnames_2d_DES):
+            if (pair in self.pairnames_2d) | (pair in self.pairnames_3d) | (pair in self.pairnames_2d_DES) | (pair in self.pairnames_3d_DES):
                 self.observable_pairs.append(pair)
                 self.pairs_zmin.append(zmin)
                 self.pairs_zmax.append(zmax)
                 self.pairs_Nz.append(Nz)
 
 
-
-
     ############################################################################
-    def execute(self):
-        ##### Set up interpolation for HMF
+    def execute(self, HMF, scaling, covmat):
+        """Return dict with multi-obs mass functions for each pair of
+        observables."""
+        self.HMF = HMF
+        self.scaling = scaling
+        self.covmat = covmat
+        # Set up interpolation for HMF
         HMF_in = self.HMF['dNdlnM'][1:,:]
         if np.any(HMF_in==0):
             HMF_in[np.where(HMF_in==0)] = np.nextafter(0, 1)
         self.HMF_interp = RectBivariateSpline(np.log(self.HMF['z_arr'][1:]), np.log(self.HMF['M_arr']), np.log(HMF_in), kx=1, ky=1)
-        self.Delta_lnM = np.log(self.HMF['M_arr'][1]/self.HMF['M_arr'][0])
-
+        self.Delta_lnM = np.log(HMF['M_arr'][1]/self.HMF['M_arr'][0])
         # Check length of HMF mass array for compression factor
-        assert (len(self.HMF['M_arr'])-1)%self.compression==0, "HMF has non-standard shape"
-
-        ##### Pre-compute the intrinsic scatter convolutions
-        output_dict = {'M_arr': self.HMF['M_arr'][::self.compression]}
+        assert (len(HMF['M_arr'])-1)%self.compression==0, "HMF has non-standard shape"
+        # Pre-compute the intrinsic scatter convolutions
+        output_dict = {'M_arr': HMF['M_arr'][::self.compression]}
         for pair_idx,pair_name in enumerate(self.observable_pairs):
             z_arr = np.linspace(self.pairs_zmin[pair_idx], self.pairs_zmax[pair_idx], self.pairs_Nz[pair_idx])
-            if pair_name in self.pairnames_2d_DES:
+            if (pair_name in self.pairnames_2d_DES) | (pair_name in self.pairnames_3d_DES):
                 this_covmat_ = 0
             else:
                 this_covmat_ = self.covmat['cov_%s'%pair_name]
@@ -78,10 +80,8 @@ class MultiObsConvolution:
                                                               pairname=pair_name,
                                                               pair_covmat=this_covmat_,
                                                               z_arr=z_arr)
-
             output_dict['%s_z'%pair_name] = z_arr
         return output_dict
-
 
 
     def get_P_multiobs_allz(self, obsname, pairname, pair_covmat, z_arr):
@@ -101,12 +101,12 @@ class MultiObsConvolution:
             argin = zip([self]*len(z_arr), z_arr)
             P_obs_grid = np.array(pool.map(unwrap_self_f, argin, chunksize=len(z_arr)//self.NPROC))
             pool.close()
-
         return P_obs_grid
 
 
     def get_P_multiobs_z(self, z):
-        """Decide whether it's a 2D or 3D observable array."""
+        """Decide whether it's a 2D or 3D observable array or whether it's the
+        fancy DES stuff."""
         # Unpack self (again, because of multiprocessing)
         covmat = self.pair_covmat
         obsname = self.obsname
@@ -118,56 +118,51 @@ class MultiObsConvolution:
             return self.get_P_2obs_DES_z(obsname, z)
         elif pairname in self.pairnames_3d:
             return self.get_P_3obs_z(obsname, covmat, z)
+        elif pairname in self.pairnames_3d_DES:
+            return self.get_P_3obs_DES_z(obsname, z)
 
 
     def get_Nbins_array(self, std):
-        Nbins_obs = int(2 * self.N_sigma * std / self.Delta_lnM)
-        if Nbins_obs%2 != 0:
-            Nbins_obs+= 1
-        if Nbins_obs<4:
-            Nbins_obs = 4
-        minmax_ = (Nbins_obs-1)/2 * self.Delta_lnM
-        lnobs_arr = np.linspace(-minmax_, minmax_, Nbins_obs)
-
-        return Nbins_obs, lnobs_arr
+        """Return number of bins and array that satisfy that std/Delta_lnM is
+        covered self.N_sigma times. 0 is Nbins_hilo[0]st element."""
+        # Number of bins below and above (without 0). At least 1
+        Nbins_hilo = (self.N_sigma * std / self.Delta_lnM).astype(int) +1
+        # We want uneven total number. Add 1 to lower if needed
+        if (Nbins_hilo[0]+Nbins_hilo[1]+1)%2 == 0:
+            Nbins_hilo[0]+= 1
+        lnobs_arr = self.Delta_lnM * np.linspace(-Nbins_hilo[0], Nbins_hilo[1], Nbins_hilo[0]+Nbins_hilo[1]+1)
+        return Nbins_hilo, lnobs_arr
 
 
     def get_P_2obs_z(self, obsname, covmat, z):
-        """Return P(obs, zeta | M, z(z_id), p) for constant correlated
+        """Return P(obs, zeta | M, z[z_id], p) for constant correlated
         scatter."""
         dN_dlnM, = np.exp(self.HMF_interp(np.log(z), np.log(self.HMF['M_arr'])))
-
         # Convert observable covmat into covmat in mass
         dlnM_dlnzeta = scaling_relations.dlnM_dlnobs('zeta', self.scaling)
         dlnM_dlnobs = scaling_relations.dlnM_dlnobs(obsname, self.scaling)
         Jacobian = np.array([[dlnM_dlnobs**2, dlnM_dlnobs*dlnM_dlnzeta],
                              [dlnM_dlnobs*dlnM_dlnzeta, dlnM_dlnzeta**2]])
         covmat_lnM = covmat * Jacobian
-
+        # Number of bins and arrays for each observable
         Nbins_obs, lnobs_arr = self.get_Nbins_array(msqrt(covmat_lnM[0,0]))
         Nbins_zeta, lnzeta_arr = self.get_Nbins_array(msqrt(covmat_lnM[1,1]))
-
         # Get the scatter kernel [lnobs, lnzeta]
-        pos = np.empty((Nbins_obs, Nbins_zeta, 2))
-        pos[:,:,0], pos[:,:,1] = np.meshgrid(lnobs_arr, lnzeta_arr, indexing='ij')
-        kernel = multivariate_normal.pdf(pos, mean=(0,0), cov=covmat_lnM)
-
-        HMF_2d = convolution.convolve_HMF_2obs_fixedkernel(dN_dlnM, kernel)
+        kernel = cy_multivariate_normal.bivariate_normal(lnobs_arr, lnzeta_arr, covmat_lnM)
+        # Convolution
+        HMF_2d = convolution.convolve_HMF_2obs_fixedkernel(dN_dlnM, kernel, Nbins_obs, Nbins_zeta)
         HMF_2d*= self.Delta_lnM
-
         # Compress
         HMF_2d = HMF_2d[::self.compression,::self.compression]
-
-        # Remove 0
-        HMF_2d[(HMF_2d==0).nonzero()] = np.nextafter(0, 1)
-
+        # Make it safe to take log
+        HMF_2d[HMF_2d==0] = np.nextafter(0,1)
         return np.log(HMF_2d)
 
 
     def get_P_2obs_DES_z(self, obsname, z):
         """Return P(DES_WL, zeta | M, z, p) with correlated scatter."""
         dN_dlnM, = np.exp(self.HMF_interp(np.log(z), np.log(self.HMF['M_arr'])))
-        ## (Mass-dependent) covariance matrices
+        # (Mass-dependent) covariance matrices
         # Main component
         cov_base = np.array([[1, self.scaling['rhoSZWL']*self.scaling['Dsz']],
                              [self.scaling['rhoSZWL']*self.scaling['Dsz'], self.scaling['Dsz']**2]])
@@ -177,7 +172,6 @@ class MultiObsConvolution:
         # DES_scatter = scaling_relations.WLscatter('wide', self.HMF['M_arr'], z, self.scaling)
         # covmat_wide = cov_base * np.array([DES_scatter**2, DES_scatter, DES_scatter, 1]).T.reshape(len(DES_scatter),2,2)
         covmat = covmat_main # + covmat_wide
-
         # Convert observable covmat into covmat in mass
         dlnM_dlnzeta = scaling_relations.dlnM_dlnobs('zeta', self.scaling)
         dlnM_dlnobs = scaling_relations.dlnM_dlnobs(obsname, self.scaling)
@@ -186,26 +180,29 @@ class MultiObsConvolution:
         covmat_lnM = covmat * Jacobian
         # Scatter kernels [lnobs, lnzeta]
         kernels = [None]*len(self.HMF['M_arr'])
+        Nbins_obs = np.empty((len(self.HMF['M_arr']), 2), dtype=int)
+        Nbins_zeta = np.empty((len(self.HMF['M_arr']), 2), dtype=int)
         for i in range(len(self.HMF['M_arr'])):
-            Nbins_obs, lnobs_arr = self.get_Nbins_array(msqrt(covmat_lnM[i,0,0]))
-            Nbins_zeta, lnzeta_arr = self.get_Nbins_array(msqrt(covmat_lnM[i,1,1]))
-            pos = np.empty((Nbins_obs, Nbins_zeta, 2))
-            pos[:,:,0], pos[:,:,1] = np.meshgrid(lnobs_arr, lnzeta_arr, indexing='ij')
-            kernels[i] = multivariate_normal.pdf(pos, mean=(0,0), cov=covmat_lnM[i])
+            # Number of bins and arrays for each observable
+            Nbins_obs[i], lnobs_arr = self.get_Nbins_array(msqrt(covmat_lnM[i,0,0]))
+            Nbins_zeta[i], lnzeta_arr = self.get_Nbins_array(msqrt(covmat_lnM[i,1,1]))
+            # Multivariate Gaussian kernel
+            kernels[i] = cy_multivariate_normal.bivariate_normal(lnobs_arr, lnzeta_arr, covmat_lnM[i])
         # Actual convolution
-        HMF_2d = convolution.convolve_HMF_2obs_varkernel(dN_dlnM, kernels)
+        HMF_2d = convolution.convolve_HMF_2obs_varkernel(dN_dlnM, kernels, Nbins_obs, Nbins_zeta)
         HMF_2d*= self.Delta_lnM
         # Compress
         HMF_2d = HMF_2d[::self.compression,::self.compression]
-        # Remove 0
-        HMF_2d[(HMF_2d==0).nonzero()] = np.nextafter(0, 1)
+        # Make it safe to take log
+        HMF_2d[HMF_2d==0] = np.nextafter(0,1)
         return np.log(HMF_2d)
 
 
     def get_P_3obs_DES_z(self, obsnames, z):
-        """Return P(DES_WL, obs[1], zeta | M, z, p) with correlated scatter."""
+        """Return P(DES_WL, obs_1, zeta | M, z, p) with correlated scatter."""
+        # Mass function at this redshift
         dN_dlnM, = np.exp(self.HMF_interp(np.log(z), np.log(self.HMF['M_arr'])))
-        ## (Mass-dependent) covariance matrices
+        # (Mass-dependent) covariance matrices
         if obsnames[1]=='richness':
             cov_base = np.array([[1, self.scaling['rhoWLrichness']*self.scaling['Drichness'], self.scaling['rhoSZWL']*self.scaling['Dsz']],
                                  [self.scaling['rhoWLrichness']*self.scaling['Drichness'], self.scaling['Drichness']**2, self.scaling['rhoSZrichness']*self.scaling['Dsz']],
@@ -213,8 +210,7 @@ class MultiObsConvolution:
         DES_scatter = scaling_relations.WLscatter('main', self.HMF['M_arr'], z, self.scaling)
         covmat = cov_base * np.array([DES_scatter**2, DES_scatter, DES_scatter,
                                       DES_scatter, np.ones(len(DES_scatter)), np.ones(len(DES_scatter)),
-                                      DES_scatter, np.ones(len(DES_scatter)), np.ones(len(DES_scatter))]).T.reshape(len(DES_scatter),2,2)
-
+                                      DES_scatter, np.ones(len(DES_scatter)), np.ones(len(DES_scatter))]).T.reshape(len(DES_scatter),3,3)
         # Convert observable covmat into covmat in mass
         dlnM_dlnzeta = scaling_relations.dlnM_dlnobs('zeta', self.scaling)
         dlnM_dlnobs = [scaling_relations.dlnM_dlnobs(obs, self.scaling) for obs in obsnames]
@@ -222,24 +218,25 @@ class MultiObsConvolution:
                              [dlnM_dlnobs[0]*dlnM_dlnobs[1], dlnM_dlnobs[1]**2,             dlnM_dlnobs[1]*dlnM_dlnzeta],
                              [dlnM_dlnobs[0]*dlnM_dlnzeta,   dlnM_dlnobs[1]*dlnM_dlnzeta,   dlnM_dlnzeta**2]])
         covmat_lnM = covmat * Jacobian
-
         # Scatter kernels [lnWL, lnobs, lnzeta]
         kernels = [None]*len(self.HMF['M_arr'])
+        Nbins_obs0 = np.empty((len(self.HMF['M_arr']), 2), dtype=int)
+        Nbins_obs1 = np.empty((len(self.HMF['M_arr']), 2), dtype=int)
+        Nbins_zeta = np.empty((len(self.HMF['M_arr']), 2), dtype=int)
         for i in range(len(self.HMF['M_arr'])):
-            Nbins_obs0, lnobs0_arr = self.get_Nbins_array(msqrt(covmat_lnM[i,0,0]))
-            Nbins_obs1, lnobs1_arr = self.get_Nbins_array(msqrt(covmat_lnM[i,0,0]))
-            Nbins_zeta, lnzeta_arr = self.get_Nbins_array(msqrt(covmat_lnM[i,1,1]))
-
-            pos = np.empty((Nbins_obs0, Nbins_obs1, Nbins_zeta, 3))
-            pos[:,:,:,0], pos[:,:,:,1], pos[:,:,:,2] = np.meshgrid(lnobs0_arr, lnobs1_arr, lnzeta_arr, indexing='ij')
-            kernels[i] = multivariate_normal.pdf(pos, mean=(0,0,0), cov=covmat_lnM[i])
+            # Number of bins and arrays for each observable
+            Nbins_obs0[i], lnobs0_arr = self.get_Nbins_array(msqrt(covmat_lnM[i,0,0]))
+            Nbins_obs1[i], lnobs1_arr = self.get_Nbins_array(msqrt(covmat_lnM[i,1,1]))
+            Nbins_zeta[i], lnzeta_arr = self.get_Nbins_array(msqrt(covmat_lnM[i,2,2]))
+            # Multivariate Gaussian kernel
+            kernels[i] = cy_multivariate_normal.trivariate_normal(lnobs0_arr, lnobs1_arr, lnzeta_arr, cov=covmat_lnM[i])
         # Actual convolution
-        HMF_3d = convolution.convolve_HMF_3obs_varkernel(dN_dlnM, kernels)
+        HMF_3d = convolution.convolve_HMF_3obs_varkernel(dN_dlnM, kernels, Nbins_obs0, Nbins_obs1, Nbins_zeta)
         HMF_3d*= self.Delta_lnM
         # Compress
-        HMF_3d = HMF_2d[::self.compression,::self.compression,::self.compression]
-        # Remove 0
-        HMF_3d[(HMF_3d==0).nonzero()] = np.nextafter(0, 1)
+        HMF_3d = HMF_3d[::self.compression,::self.compression,::self.compression]
+        # Make it safe to take log
+        HMF_3d[HMF_3d==0] = np.nextafter(0,1)
         return np.log(HMF_3d)
 
 
@@ -247,34 +244,24 @@ class MultiObsConvolution:
         """Return P(obs0, obs1, zeta | M, z(z_id), p) for constant correlated
         scatter."""
         dN_dlnM, = np.exp(self.HMF_interp(np.log(z), np.log(self.HMF['M_arr'])))
-
         # Convert observable covmat into covmat in mass
         dlnM_dlnzeta = scaling_relations.dlnM_dlnobs('zeta', self.scaling)
         dlnM_dlnobs = [scaling_relations.dlnM_dlnobs(obs, self.scaling) for obs in obsnames]
-
         Jacobian = np.array([[dlnM_dlnobs[0]**2,             dlnM_dlnobs[0]*dlnM_dlnobs[1], dlnM_dlnobs[0]*dlnM_dlnzeta],
                              [dlnM_dlnobs[0]*dlnM_dlnobs[1], dlnM_dlnobs[1]**2,             dlnM_dlnobs[1]*dlnM_dlnzeta],
                              [dlnM_dlnobs[0]*dlnM_dlnzeta,   dlnM_dlnobs[1]*dlnM_dlnzeta,   dlnM_dlnzeta**2]])
         covmat_lnM = covmat * Jacobian
-
-
+        # Number of bins and observable arrays
         Nbins_obs0, lnobs0_arr = self.get_Nbins_array(msqrt(covmat_lnM[0,0]))
         Nbins_obs1, lnobs1_arr = self.get_Nbins_array(msqrt(covmat_lnM[1,1]))
         Nbins_zeta, lnzeta_arr = self.get_Nbins_array(msqrt(covmat_lnM[2,2]))
-
-
         # Get the scatter kernel [lnobs, lnzeta]
-        pos = np.empty((Nbins_obs0, Nbins_obs1, Nbins_zeta, 3))
-        pos[:,:,:,0], pos[:,:,:,1], pos[:,:,:,2] = np.meshgrid(lnobs0_arr, lnobs1_arr, lnzeta_arr, indexing='ij')
-        kernel = multivariate_normal.pdf(pos, mean=(0,0,0), cov=covmat)
-
-        HMF_3d = convolution.convolve_HMF_3obs_fixedkernel(dN_dlnM, kernel)
+        kernel = cy_multivariate_normal.trivariate_normal(lnobs0_arr, lnobs1_arr, lnzeta_arr, covmat)
+        # Convolution
+        HMF_3d = convolution.convolve_HMF_3obs_fixedkernel(dN_dlnM, kernel, Nbins_obs0, Nbins_obs1, Nbins_zeta)
         HMF_3d*= self.Delta_lnM
-
         # Compress
         HMF_3d = HMF_3d[::self.compression,::self.compression,::self.compression]
-
-        # Remove 0
-        HMF_3d[(HMF_3d==0).nonzero()] = np.nextafter(0, 1)
-
+        # Make it safe to take log
+        HMF_3d[HMF_3d==0] = np.nextafter(0,1)
         return np.log(HMF_3d)
--- convolution.py
+++ convolution.py
@@ -2,91 +2,92 @@ from __future__ import division
 import numpy as np
 
 
-
-def convolve_HMF_2obs_fixedkernel(dN_dlnM, kernel):
+def convolve_HMF_2obs_fixedkernel(dN_dlnM, kernel, Nbins_0, Nbins_1):
     """Convolve 1d HMF into the 2d obs-obs space for a fixed kernel.
     Normalization is not conserved!"""
-    kernel_shape = kernel.shape
-    assert kernel_shape[0]%2==0, "Kernel must be of even length!"
-    assert kernel_shape[1]%2==0, "Kernel must be of even length!"
+    kernel_shape = np.array(kernel.shape)
+    if __debug__:
+        assert np.all(kernel_shape%2==1), "Kernel must be of uneven length"
+        assert np.all(kernel_shape-[Nbins_0[0]+Nbins_0[1], Nbins_1[0]+Nbins_1[1]]==1), "Kernel and Nbins do not match"
     len_HMF = len(dN_dlnM)
-    shape_out = np.array([len_HMF, len_HMF]) + np.array(kernel_shape)
+    shape_out = np.array([len_HMF, len_HMF]) + [Nbins_0[0]+Nbins_0[1], Nbins_1[0]+Nbins_1[1]]
     res = np.zeros(shape_out)
     for i in range(len_HMF):
         res[i:i+kernel_shape[0], i:i+kernel_shape[1]]+= dN_dlnM[i] * kernel
-    res_out = res[kernel_shape[0]//2:-kernel_shape[0]//2, kernel_shape[1]//2:-kernel_shape[1]//2]
+    res_out = res[Nbins_0[0]:-Nbins_0[1], Nbins_1[0]:-Nbins_1[1]]
     return res_out
 
 
-def convolve_HMF_3obs_fixedkernel(dN_dlnM, kernel):
+def convolve_HMF_3obs_fixedkernel(dN_dlnM, kernel, Nbins_0, Nbins_1, Nbins_2):
     """Convolve 1d HMF into the 3d obs-obs-obs space for a fixed kernel.
     Normalization is not conserved!"""
     kernel_shape = kernel.shape
-    assert kernel_shape[0]%2==0, "Kernel must be of even length!"
-    assert kernel_shape[1]%2==0, "Kernel must be of even length!"
-    assert kernel_shape[2]%2==0, "Kernel must be of even length!"
-    _len_HMF = len(dN_dlnM)
-    shape_out = np.array([_len_HMF, _len_HMF, _len_HMF]) + np.array(kernel_shape)
+    if __debug__:
+        assert np.all(kernel_shape%2==1), "Kernel must be of uneven length"
+        assert np.all(kernel_shape-[Nbins_0[0]+Nbins_0[1], Nbins_1[0]+Nbins_1[1], Nbins_2[0]+Nbins_2[1]]==1), "Kernel and Nbins do not match"
+    len_HMF = len(dN_dlnM)
+    shape_out = np.array([len_HMF, len_HMF, len_HMF]) + [Nbins_0[0]+Nbins_0[1], Nbins_1[0]+Nbins_1[1], Nbins_2[0]+Nbins_2[1]]
     res = np.zeros(shape_out)
-    for i in range(_len_HMF):
+    for i in range(len_HMF):
         res[i:i+kernel_shape[0], i:i+kernel_shape[1], i:i+kernel_shape[2]]+= dN_dlnM[i] * kernel
-    res_out = res[kernel_shape[0]//2:-kernel_shape[0]//2, kernel_shape[1]//2:-kernel_shape[1]//2, kernel_shape[2]//2:-kernel_shape[2]//2]
+    res_out = res[Nbins_0[0]:-Nbins_0[1], Nbins_1[0]:-Nbins_1[1], Nbins_2[0]:-Nbins_2[1]]
     return res_out
 
 
-def convolve_HMF_2obs_varkernel(dN_dlnM, kernels):
+def convolve_HMF_2obs_varkernel(dN_dlnM, kernels, Nbins_0, Nbins_1):
     """Convolve 1d HMF into the 2d obs-obs space for varying kernels.
     Normalization is not conserved!"""
-    # Validate input
     len_HMF = len(dN_dlnM)
-    assert len(kernels)==len_HMF, "Need as many kernels as entries in HMF"
-    kernel_shapes = np.array([kernels[i].shape for i in range(len_HMF)])
-    assert np.all(kernel_shapes%2==0), "Kernels must be of even length"
+    kernel_shapes = np.array([kernels[i].shape for i in range(len(kernels))])
+    if __debug__:
+        assert len(kernels)==len_HMF, "Need as many kernels as entries in HMF"
+        assert np.all(kernel_shapes%2==1), "Kernels must be of uneven length"
     # Compute padding
-    buffer_0_lo = np.amax(kernel_shapes[:,0]//2-range(len_HMF))
-    buffer_0_hi = np.amax(kernel_shapes[:,0]//2-range(len_HMF)[::-1])
-    buffer_1_lo = np.amax(kernel_shapes[:,1]//2-range(len_HMF))
-    buffer_1_hi = np.amax(kernel_shapes[:,1]//2-range(len_HMF)[::-1])
-    shape_out = [len_HMF+buffer_0_lo+buffer_0_hi, len_HMF+buffer_1_lo+buffer_1_hi]
+    pad_0_lo = np.amax(Nbins_0[:,0]-range(len_HMF))
+    pad_0_hi = np.amax(Nbins_0[:,1]-range(len_HMF)[::-1])
+    pad_1_lo = np.amax(Nbins_1[:,0]-range(len_HMF))
+    pad_1_hi = np.amax(Nbins_1[:,1]-range(len_HMF)[::-1])
+
+    shape_out = [len_HMF+pad_0_lo+pad_0_hi, len_HMF+pad_1_lo+pad_1_hi]
     # Do the convolution
     res = np.zeros(shape_out)
     for i in range(len_HMF):
-        idx_0_lo = i+buffer_0_lo-kernel_shapes[i][0]//2
-        idx_0_hi = i+buffer_0_lo+kernel_shapes[i][0]//2
-        idx_1_lo = i+buffer_1_lo-kernel_shapes[i][1]//2
-        idx_1_hi = i+buffer_1_lo+kernel_shapes[i][1]//2
+        idx_0_lo = i+pad_0_lo-Nbins_0[i,0]
+        idx_0_hi = i+pad_0_lo+Nbins_0[i,1]+1
+        idx_1_lo = i+pad_1_lo-Nbins_1[i,0]
+        idx_1_hi = i+pad_1_lo+Nbins_1[i,1]+1
         res[idx_0_lo:idx_0_hi, idx_1_lo:idx_1_hi]+= dN_dlnM[i] * kernels[i]
     # Remove padding
-    res_out = res[buffer_0_lo:-buffer_0_hi, buffer_1_lo:-buffer_1_hi]
+    res_out = res[pad_0_lo:-pad_0_hi, pad_1_lo:-pad_1_hi]
     return res_out
 
 
-def convolve_HMF_3obs_varkernel(dN_dlnM, kernels):
+def convolve_HMF_3obs_varkernel(dN_dlnM, kernels, Nbins_0, Nbins_1, Nbins_2):
     """Convolve 1d HMF into the 3d obs space for varying kernels.
     Normalization is not conserved!"""
-    # Validate input
     len_HMF = len(dN_dlnM)
-    assert len(kernels)==len_HMF, "Need as many kernels as entries in HMF"
-    kernel_shapes = np.array([kernels[i].shape for i in range(len_HMF)])
-    assert np.all(kernel_shapes%2==0), "Kernels must be of even length"
+    kernel_shapes = np.array([kernels[i].shape for i in range(len(kernels))])
+    if __debug__:
+        assert len(kernels)==len_HMF, "Need as many kernels as entries in HMF"
+        assert np.all(kernel_shapes%2==1), "Kernels must be of uneven length"
     # Compute padding
-    buffer_0_lo = np.amax(kernel_shapes[:,0]//2-range(len_HMF))
-    buffer_0_hi = np.amax(kernel_shapes[:,0]//2-range(len_HMF)[::-1])
-    buffer_1_lo = np.amax(kernel_shapes[:,1]//2-range(len_HMF))
-    buffer_1_hi = np.amax(kernel_shapes[:,1]//2-range(len_HMF)[::-1])
-    buffer_2_lo = np.amax(kernel_shapes[:,2]//2-range(len_HMF))
-    buffer_2_hi = np.amax(kernel_shapes[:,2]//2-range(len_HMF)[::-1])
-    shape_out = [len_HMF+buffer_0_lo+buffer_0_hi, len_HMF+buffer_1_lo+buffer_1_hi, len_HMF+buffer_2_lo+buffer_2_hi]
+    pad_0_lo = np.amax(Nbins_0[:,0]-range(len_HMF))
+    pad_0_hi = np.amax(Nbins_0[:,1]-range(len_HMF)[::-1])
+    pad_1_lo = np.amax(Nbins_1[:,0]-range(len_HMF))
+    pad_1_hi = np.amax(Nbins_1[:,1]-range(len_HMF)[::-1])
+    pad_2_lo = np.amax(Nbins_2[:,0]-range(len_HMF))
+    pad_2_hi = np.amax(Nbins_2[:,1]-range(len_HMF)[::-1])
+    shape_out = [len_HMF+pad_0_lo+pad_0_hi, len_HMF+pad_1_lo+pad_1_hi, len_HMF+pad_2_lo+pad_2_hi]
     # Do the convolution
     res = np.zeros(shape_out)
     for i in range(len_HMF):
-        idx_0_lo = i+buffer_0_lo-kernel_shapes[i][0]//2
-        idx_0_hi = i+buffer_0_lo+kernel_shapes[i][0]//2
-        idx_1_lo = i+buffer_1_lo-kernel_shapes[i][1]//2
-        idx_1_hi = i+buffer_1_lo+kernel_shapes[i][1]//2
-        idx_2_lo = i+buffer_2_lo-kernel_shapes[i][2]//2
-        idx_2_hi = i+buffer_2_lo+kernel_shapes[i][2]//2
+        idx_0_lo = i+pad_0_lo-Nbins_0[i,0]
+        idx_0_hi = i+pad_0_lo+Nbins_0[i,1]+1
+        idx_1_lo = i+pad_1_lo-Nbins_1[i,0]
+        idx_1_hi = i+pad_1_lo+Nbins_1[i,1]+1
+        idx_2_lo = i+pad_2_lo-Nbins_2[i,0]
+        idx_2_hi = i+pad_2_lo+Nbins_2[i,1]+1
         res[idx_0_lo:idx_0_hi, idx_1_lo:idx_1_hi, idx_2_lo:idx_2_hi]+= dN_dlnM[i] * kernels[i]
     # Remove padding
-    res_out = res[buffer_0_lo:-buffer_0_hi, buffer_1_lo:-buffer_1_hi, buffer_2_lo:-buffer_2_hi]
+    res_out = res[pad_0_lo:-pad_0_hi, pad_1_lo:-pad_1_hi, pad_2_lo:-pad_2_hi]
     return res_out
