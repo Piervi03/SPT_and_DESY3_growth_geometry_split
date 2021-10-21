eb767da479dbccc199f009183a7de3fa007153b8
Sebastian Bocquet <sebastian.bocquet@gmail.com>
2021-10-21 11:26:41 +0200

    generic_miscenter as standalone function

--- miscentering.py
+++ miscentering.py
@@ -3,6 +3,14 @@ import numpy as np
 
 import cosmo
 
+def generic_miscenter(params, lam, z): 
+    """Return parameters of generic intrinsic miscentering in units [Mpc/h]."""
+    alpha = params['alpha_0'] * ((1+z)/1.5)**params['alpha_z'] * (lam/60)**params['alpha_lam']
+    mis_phys_0 = params['comp0_0'] * ((1+z)/1.5)**params['comp0_z'] * (lam/60)**params['comp0_lam']
+    Delta_mis_phys = params['comp1_0'] * ((1+z)/1.5)**params['comp1_z'] * (lam/60)**params['comp1_lam']
+    mis_phys_1 = mis_phys_0 + Delta_mis_phys
+    return alpha, mis_phys_0, mis_phys_1
+
 class MisCentering(object):
 
     def __init__(self, opt):
@@ -24,7 +32,7 @@ class MisCentering(object):
 
     def get_mean_Rmis_optical(self, cluster):
         """Mean miscentering [Mpc/h] for optical centers."""
-        rho, sigma_0, sigma_1 = self.generic_miscenter(self.opt, cluster['richness'], cluster['REDSHIFT'])
+        rho, sigma_0, sigma_1 = generic_miscenter(self.opt, cluster['richness'], cluster['REDSHIFT'])
         R = np.sqrt(np.pi/2) * (rho*sigma_0 + (1-rho)*sigma_1)
         return R
 
@@ -33,17 +41,9 @@ class MisCentering(object):
         intrinsic SZ miscentering."""
         sigma_obs_arcmin = np.sqrt(1.3**2 + self.opt['kappa_SPT']**2 * cluster['THETA_CORE']**2)/cluster['XI']
         sigma_obs_Mpch = sigma_obs_arcmin / 60 * np.pi/180 * cosmo.dA(cluster['REDSHIFT'], cosmology)
-        rho, sigma_0, sigma_1 = self.generic_miscenter(self.opt, cluster['richness'], cluster['REDSHIFT'])
+        rho, sigma_0, sigma_1 = generic_miscenter(self.opt, cluster['richness'], cluster['REDSHIFT'])
         sigma_0 = np.sqrt(sigma_0**2 + sigma_obs_Mpch**2)
         sigma_1 = np.sqrt(sigma_1**2 + sigma_obs_Mpch**2)
         R = np.sqrt(np.pi/2) * (rho*sigma_0 + (1-rho)*sigma_1)
         return R
 
-    def generic_miscenter(self, params, lam, z):
-        """Return parameters of generic intrinsic miscentering in units [Mpc/h]."""
-        r500 = (lam/60)**(params['B_lambda']/3) * ((1+z)/1.5)**params['C_lambda']
-        alpha = params['alpha_0'] * ((1+z)/1.5)**params['alpha_z'] * (lam/60)**params['alpha_lam']
-        mis_phys_0 = r500 * params['comp0_0'] * ((1+z)/1.5)**params['comp0_z'] * (lam/60)**params['comp0_lam']
-        Delta_mis_phys = r500 * params['comp1_0'] * ((1+z)/1.5)**params['comp1_z'] * (lam/60)**params['comp1_lam']
-        mis_phys_1 = mis_phys_0 + Delta_mis_phys
-        return alpha, mis_phys_0, mis_phys_1
