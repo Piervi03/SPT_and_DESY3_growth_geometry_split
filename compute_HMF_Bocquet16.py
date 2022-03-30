31e402b2654196de7033c59565885090c714cf76
Sebastian Bocquet <sebastian.bocquet@gmail.com>
2022-03-30 16:06:41 +0200

    Bocquet+16 mass function

---
+++ compute_HMF_Bocquet16.py
@@ -0,0 +1,83 @@
+from __future__ import division
+import numpy as np
+import imp
+from scipy.interpolate import interp1d
+import scipy.integrate
+import cosmo
+
+class HMFCalculator:
+    def __init__(self, Deltacrit):
+        assert Deltacrit in [200., 500.], "Deltacrit must be 200. or 500., not %f"%Deltacrit
+        self.Deltacrit = Deltacrit
+
+
+    def get_factors(self, Omega_m, z):
+        """Return the universality correction factors."""
+        if self.Deltacrit==200.:
+            gamma0 = 3.54e-2 + Omega_m**.09
+            gamma1 = 4.56e-2 + 2.68e-2/Omega_m
+            gamma2 = 0.721 + 3.5e-2/Omega_m
+            gamma3 = .628 + .164/Omega_m
+            delta0 = -1.67e-2 + 2.18e-2*Omega_m
+            delta1 = 6.52e-3 - 6.86e-3*Omega_m
+            alpha = gamma0 + gamma1* np.exp(-((gamma2-z)/gamma3)**2)
+            beta = delta0 + delta1*z
+        elif self.Deltacrit==500.:
+            alpha0 = .88 + .329*Omega_m
+            alpha1 = 1. + 4.31e-2/Omega_m
+            alpha2 = -.365 + .254/Omega_m
+            alpha = alpha0* (alpha1*z+alpha2)/(z+alpha2)
+            beta = -1.7e-2 + 3.74e-3*Omega_m
+        return alpha, beta
+        
+
+    def get_params(self, z):
+        """Return the mass function parameters as function of redshift."""
+        if self.Deltacrit==200.:
+            z0params = np.array([.222, 1.71, 2.24, 1.46])
+            zparams = np.array([.269, .321, -.621, -.153])
+        elif self.Deltacrit==500.:
+            z0params = np.array([.241, 2.18, 2.35, 2.02])
+            zparams = np.array([.370, .251, -.698, -.310])
+        return z0params[:,None] * (1+z[None,:])**zparams[:,None]
+
+
+    def compute_HMF(self, cosmology, z_arr, k_arr, Pk):
+        """Compute Bocquet et al. (2016) HMF and apply redshift volume."""
+        ##### Setup
+        rho_m = (cosmology['Omega_m'] - cosmology['Omega_nu']) * cosmo.RHOCRIT
+        # Data arrays
+        M_arr = np.logspace(13, 16, 301)
+
+        ##### Compute sigma(M)
+        # Radius [M_arr]
+        R = (3 * M_arr / (4 * np.pi * rho_m))**(1/3)
+        # [M_arr, k_arr]
+        kR = k_arr[None,:] * R[:,None]
+        # Window functions [M_arr, k_arr]
+        window = 3 * (np.sin(kR)/kR**3 - np.cos(kR)/kR**2)
+        dwindow = 3/kR**4 * (3*kR*np.cos(kR) + ((kR**2 - 3)*np.sin(kR)))
+        # Integrands [z_arr, M_arr, k_arr]
+        integrand_sigma2 = Pk[:,None,:] * window[None,:,:]**2 * k_arr[None,None,:]**3
+        integrand_dsigma2dM = Pk[:,None,:] * window[None,:,:] * dwindow[None,:,:] * k_arr[None,None,:]**4
+        # Sigma^2 and dsigma^2/dM [z_arr, M_arr]
+        sigma2 = .5/np.pi**2 * np.trapz(integrand_sigma2, np.log(k_arr), axis=-1)
+        dsigma2dM = np.pi**-2 * R[None,:]/M_arr[None,:]/3 * np.trapz(integrand_dsigma2dM, np.log(k_arr), axis=-1)
+
+        ##### Compute HMF (unit volume) [z_arr, M_arr]
+        A, a, b, c = self.get_params(z_arr)
+        fsigma = A[:,None] * ((np.sqrt(sigma2)/b[:,None])**-a[:,None] + 1) * np.exp(-c[:,None]/sigma2)
+        dNdlnM_noVol = - fsigma * rho_m * dsigma2dM/2/sigma2
+        # Universality correction
+        alpha, beta = self.get_factors(cosmology['Omega_m'], z_arr)
+        dNdlnM_noVol*= alpha[:,None] + beta[:,None]*np.log(M_arr/cosmology['h'])[None,:]
+
+        ##### Apply redshift volume
+        deltaV = cosmo.deltaV(z_arr[1:], cosmology)
+        deltaV = np.insert(deltaV, 0, deltaV[1])
+        dNdlnM = dNdlnM_noVol * deltaV[:,None]
+
+        ##### Return HMF
+        return M_arr, dNdlnM_noVol, dNdlnM
+
+
---
+++ compute_HMF_Bocquet16_interface.py
@@ -0,0 +1,69 @@
+from __future__ import division
+import numpy as np
+import xarray as xr
+import os
+
+from cosmosis.datablock import option_section
+
+import compute_HMF_Bocquet16 as compute_HMF
+
+
+class EmptyClass:
+    pass
+
+def setup(options):
+    # Print repository status
+    path_to_repo = os.path.dirname(__file__)
+    os.system("git --git-dir=%s/.git --work-tree=%s status"%(path_to_repo, path_to_repo))
+    os.system("git --git-dir=%s/.git --work-tree=%s "%(path_to_repo, path_to_repo)+"show -s --format=%h")
+    # Proceed with actual setup
+    recalc_HMF = options.get_bool(option_section, 'recalc_HMF', default=True)
+    save_HMF_to_disk = options.get_bool(option_section, 'save_HMF_to_disk', default=False)
+    Deltacrit = options.get_double(option_section, 'Deltacrit', default=500.)
+    
+    if recalc_HMF:
+        HMF_calculator = compute_HMF.HMFCalculator(Deltacrit)
+    else:
+        HMF_calculator = EmptyClass()
+        HMF_calculator.HMF = xr.open_dataset('HMF.nc')
+    HMF_calculator.recalc_HMF = recalc_HMF
+    HMF_calculator.save_HMF_to_disk = save_HMF_to_disk
+    return HMF_calculator
+
+def execute(block, HMF_calculator):
+    if HMF_calculator.recalc_HMF:
+        # Only need cosmo for E(z)-type stuff
+        cosmology = {
+            'Omega_m': block.get_double('cosmological_parameters', 'Omega_m'),
+            'Omega_nu': block.get_double('cosmological_parameters', 'Omega_nu'),
+            'Omega_l': block.get_double('cosmological_parameters', 'omega_lambda'),
+            'w0': block.get_double('cosmological_parameters', 'w'),
+            'wa': block.get_double('cosmological_parameters', 'wa'),
+            'h': block.get_double('cosmological_parameters', 'hubble')/100.}
+        # Matter power spectrum
+        z_arr = block.get_double_array_1d('matter_power_lin_cdm_baryon', 'z')
+        k_arr = block.get_double_array_1d('matter_power_lin_cdm_baryon', 'k_h')
+        Pk = block.get_double_array_nd('matter_power_lin_cdm_baryon', 'p_k')
+        # Compute the HMF
+        M_arr, dNdlnM_noVol, dNdlnM = HMF_calculator.compute_HMF(cosmology, z_arr, k_arr, Pk)
+        # Put it into block
+        block.put_double_array_1d('HMF', 'M_arr', M_arr)
+        block.put_double_array_1d('HMF', 'z_arr', z_arr)
+        block.put_double_array_nd('HMF', 'dNdlnM_unitVol', dNdlnM_noVol)
+        block.put_double_array_nd('HMF', 'dNdlnM', dNdlnM)
+        if HMF_calculator.save_HMF_to_disk:
+            HMF = xr.DataArray(block.get_double_array_nd('HMF', 'dNdlnM'),
+                               dims=['z', 'm'],
+                               coords={'z': block.get_double_array_1d('HMF', 'z_arr'),
+                                       'm': block.get_double_array_1d('HMF', 'M_arr')})
+            HMF.to_netcdf('HMF.nc')
+
+    else:
+        block.put_double_array_1d('HMF', 'M_arr', np.array(HMF_calculator.HMF['m']))
+        block.put_double_array_1d('HMF', 'z_arr', np.array(HMF_calculator.HMF['z']))
+        dNdlnM_ = np.array(HMF_calculator.HMF.to_array()[0])
+        block.put_double_array_nd('HMF', 'dNdlnM', dNdlnM_)
+    return 0
+
+def cleanup(config):
+    pass
