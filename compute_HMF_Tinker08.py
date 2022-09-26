9e0134d19b865bebffdbd1b67c779b88c3be959b
Sebastian Bocquet <sebastian.bocquet@gmail.com>
2022-09-26 14:07:44 +0200

    compute volume element and HMF parameters on fine redshift array

--- compute_HMF_Tinker08.py
+++ compute_HMF_Tinker08.py
@@ -1,14 +1,16 @@
 from __future__ import division
 import numpy as np
 import imp
-from scipy.interpolate import interp1d
+from scipy.interpolate import interp1d, RectBivariateSpline
 import scipy.integrate
 import cosmo
 
 class HMFCalculator:
-    def __init__(self, Deltacrit):
+    def __init__(self, Deltacrit, z_arr, M_arr):
         """Initialize Tinker parameter interpolation functions."""
         self.Deltacrit = Deltacrit
+        self.z_arr = z_arr
+        self.M_arr = M_arr
         # Initialize Tinker interpolation (A, a, b, c)
         x = np.log((200., 300., 400., 600., 800., 1200., 1600., 2400., 3200.))
         y = (1.858659e-01, 1.995973e-01, 2.115659e-01, 2.184113e-01, 2.480968e-01, 2.546053e-01, 2.600000e-01, 2.600000e-01, 2.600000e-01)
@@ -21,43 +23,42 @@ class HMFCalculator:
         self.interp_c = interp1d(x, y, kind='cubic')
 
 
-    def compute_HMF(self, cosmology, z_arr, k_arr, Pk):
+    def compute_HMF(self, cosmology, z, k, Pk):
         """Compute Tinker HMF and apply redshift volume."""
         ##### Setup
         rho_m = (cosmology['Omega_m'] - cosmology['Omega_nu']) * cosmo.RHOCRIT
-        # Data arrays
-        M_arr = np.logspace(13, 16, 301)
-
         # Mean overdensity at each redshift
-        Deltamean = self.Deltacrit / cosmo.Omega_m_z(z_arr, cosmology)
+        Deltamean = self.Deltacrit / cosmo.Omega_m_z(self.z_arr, cosmology)
 
         ##### Compute sigma(M)
         # Radius [M_arr]
-        R = (3 * M_arr / (4 * np.pi * rho_m))**(1/3)
-        # [M_arr, k_arr]
-        kR = k_arr[None,:] * R[:,None]
-        # Window functions [M_arr, k_arr]
+        R = (3 * self.M_arr / (4 * np.pi * rho_m))**(1/3)
+        # [M_arr, k]
+        kR = k[None,:] * R[:,None]
+        # Window functions [M_arr, k]
         window = 3 * (np.sin(kR)/kR**3 - np.cos(kR)/kR**2)
         dwindow = 3/kR**4 * (3*kR*np.cos(kR) + ((kR**2 - 3)*np.sin(kR)))
-        # Integrands [z_arr, M_arr, k_arr]
-        integrand_sigma2 = Pk[:,None,:] * window[None,:,:]**2 * k_arr[None,None,:]**3
-        integrand_dsigma2dM = Pk[:,None,:] * window[None,:,:] * dwindow[None,:,:] * k_arr[None,None,:]**4
+        # Integrands [z_arr, M_arr, k]
+        integrand_sigma2 = Pk[:,None,:] * window[None,:,:]**2 * k[None,None,:]**3
+        integrand_dsigma2dM = Pk[:,None,:] * window[None,:,:] * dwindow[None,:,:] * k[None,None,:]**4
         # Sigma^2 and dsigma^2/dM [z_arr, M_arr]
-        sigma2 = .5/np.pi**2 * np.trapz(integrand_sigma2, np.log(k_arr), axis=-1)
-        dsigma2dM = np.pi**-2 * R[None,:]/M_arr[None,:]/3 * np.trapz(integrand_dsigma2dM, np.log(k_arr), axis=-1)
+        sigma2 = .5/np.pi**2 * np.trapz(integrand_sigma2, np.log(k), axis=-1)
+        dsigma2dM = np.pi**-2 * R[None,:]/self.M_arr[None,:]/3 * np.trapz(integrand_dsigma2dM, np.log(k), axis=-1)
+        sigma2_fine = np.exp(RectBivariateSpline(z, self.M_arr, np.log(sigma2), kx=1, ky=1)(self.z_arr, self.M_arr))
+        dsigma2dM_fine = -np.exp(RectBivariateSpline(z, self.M_arr, np.log(-dsigma2dM), kx=1, ky=1)(self.z_arr, self.M_arr))
 
         ##### Compute Tinker HMF (unit volume)
-        A, a, b, c = np.array([self.Tinker_params(z_arr[i], Deltamean[i]) for i in range(len(z_arr))]).T
+        A, a, b, c = np.array([self.Tinker_params(self.z_arr[i], Deltamean[i]) for i in range(len(self.z_arr))]).T
         # HMF [z_arr, M_arr]
-        fsigma = A[:,None] * ((np.sqrt(sigma2)/b[:,None])**-a[:,None] + 1) * np.exp(-c[:,None]/sigma2)
-        dNdlnM_noVol = - fsigma * rho_m * dsigma2dM/2/sigma2
+        fsigma = A[:,None] * ((np.sqrt(sigma2_fine)/b[:,None])**-a[:,None] + 1) * np.exp(-c[:,None]/sigma2_fine)
+        dNdlnM_noVol = - fsigma * rho_m * dsigma2dM_fine/2/sigma2_fine
 
         ##### Apply redshift volume
-        deltaV = cosmo.deltaV(z_arr, cosmology)
+        deltaV = cosmo.deltaV(self.z_arr, cosmology)
         dNdlnM = dNdlnM_noVol * deltaV[:,None]
 
         ##### Return HMF
-        return M_arr, dNdlnM_noVol, dNdlnM
+        return dNdlnM_noVol, dNdlnM
 
 
 
