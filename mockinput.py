4bb4c623a894a01d721a8c4895063cebfa436d91
Sebastian Bocquet <sebastian.bocquet@gmail.com>
2023-03-13 21:55:37 +0100

    update mockgenerator, too

--- mockinput.py
+++ mockinput.py
@@ -1,4 +1,5 @@
 import numpy as np
+import h5py
 
 SPT_survey = 'data/SPT_SZ_ECS_500d_survey.txt'
 MCMF_lambda_min = 'data/MCMF_lambda_min.txt'
@@ -29,8 +30,8 @@ scaling = {'Asz': .96, 'Bsz': 1.5, 'Csz': .5, 'Dsz': .2, 'zeta_min': 1.,
            'DWL_HST': .3,
 
            'DES_m_piv': 2e14,
-           'DES_b_dev_0': 0,  'DES_b_dev_1': 0., 'DES_b_dev_2': 0., 'DES_b_dev_m': 0.,
-           'DES_s_dev_0': 0., 'DES_s_dev_1': 0., 'DES_s_dev_2': 0, 'DES_s_dev_m': 0.,
+           'DES_b_dev_1': 0., 'DES_b_dev_2': 0., 'DES_b_dev_m': 0.,
+           'DES_s_dev': 0., 'DES_s_dev_m': 0.,
 
            'Adisp':939., 'Bdisp':2.91, 'Cdisp':.33, 'Ddisp0':.2, 'DdispN':3.,
            'Arichness': 4.25, 'Brichness': 1., 'Crichness': 0., 'Drichness': .2,
@@ -44,16 +45,11 @@ scaling = {'Asz': .96, 'Bsz': 1.5, 'Csz': .5, 'Dsz': .2, 'zeta_min': 1.,
            'richmPivot': 3e14,
            'YXPARAM': 'SPT_XVP',
            }
-tmp = np.loadtxt('data/MCMF_dnf_500.txt')
-scaling['DESwl_z'] = [.252, .470, .783,]
-scaling['DESwl_bias_mean'] = np.mean(tmp[:,:3], axis=0)
-scaling['DESwl_bias_m_mean'] = np.mean(tmp[:,3])
-scaling['DESwl_bias_std'] = np.std(tmp[:,:3], axis=0)
-scaling['DESwl_bias_m_std'] = np.std(tmp[:,3])
-scaling['DESwl_scatter_mean'] = np.mean(tmp[:,4:7], axis=0)
-scaling['DESwl_scatter_m_mean'] = np.mean(tmp[:,7])
-scaling['DESwl_scatter_std'] = np.std(tmp[:,4:7], axis=0)
-scaling['DESwl_scatter_m_std'] = np.std(tmp[:,7])
+DES_WL_priors_file = './data/WLcalib_MCMF_dnf_500kpch.h5'
+with h5py.File(DES_WL_priors_file, 'r') as f:
+    DES_WL_prior = {}
+    for k in f.keys():
+        scaling['DES_%s'%k] = f[k][()]
 
 # SPT survey cuts
 surveyCutRedshift = (0.25, 1.78)
