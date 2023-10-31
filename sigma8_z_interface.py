8f4e76b45477fa4445c663bf562a0ed3dd0c96c1
Sebastian Bocquet <sebastian.bocquet@gmail.com>
2023-10-31 09:54:39 +0100

    Option to only compute sigma_8(z)

--- sigma8_z_interface.py
+++ sigma8_z_interface.py
@@ -6,22 +6,27 @@ import sigma8_z
 
 def setup(options):
     """Only need to read redshift bins and centers from config file."""
+    rescale = options.get_bool(option_section, 'rescale', default=False)
     zlim = options.get_double_array_1d(option_section, 'zlim')
     zmid = options.get_double_array_1d(option_section, 'zmid')
-    return zlim, zmid
+    return rescale, zlim, zmid
 
 def execute(block, setup_stuff):
     """Replace power spectrum with rescaled version, and store array of
     sigma_8(z) for plotting."""
     # Input parameters
-    zlim, zmid = setup_stuff
-    p_sigma8_z = np.array([block.get_double('cosmological_parameters', 'sigma8_z_%d'%i) for i in range(len(zmid))])
+    rescale, zlim, zmid = setup_stuff
+    if rescale:
+        p_sigma8_z = np.array([block.get_double('cosmological_parameters', 'sigma8_z_%d'%i) for i in range(len(zmid))])
+    else:
+        p_sigma8_z = None
     # cdm+bar power spectrum (w/o neutrinos)
     z, k, Pk = block.get_grid('cdm_baryon_power_lin', 'z', 'k_h', 'p_k')
     # Rescale matter power spectrum
-    Pk, z_out, sigma8_zout = sigma8_z.rescale_Pk(z, k, Pk, zlim, zmid, p_sigma8_z)
+    Pk, z_out, sigma8_zout = sigma8_z.rescale_Pk(z, k, Pk, zlim, zmid, p_sigma8_z, rescale)
     # Write to cosmosis block
-    block.replace_double_array_nd('cdm_baryon_power_lin', 'p_k', Pk)
+    if rescale:
+        block.replace_double_array_nd('cdm_baryon_power_lin', 'p_k', Pk)
     for i in range(len(sigma8_zout)):
         block.put_double('cosmological_parameters', 'sigma8_z_out_%d'%i, sigma8_zout[i])
     return 0
