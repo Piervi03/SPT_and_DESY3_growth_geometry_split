de9e12ce6340d4f9671263dbd392ea2a0c31481c
Sebastian Bocquet <sebastian.bocquet@gmail.com>
2024-05-10 20:58:56 +0200

    Add Tinker+10 HMF

--- compute_HMF_Tinker08.py
+++ compute_HMF_Tinker08.py
@@ -12,13 +12,13 @@ class HMFCalculator:
         # Initialize Tinker interpolation (A, a, b, c)
         x = np.log((200., 300., 400., 600., 800., 1200., 1600., 2400., 3200.))
         y = (1.858659e-01, 1.995973e-01, 2.115659e-01, 2.184113e-01, 2.480968e-01, 2.546053e-01, 2.600000e-01, 2.600000e-01, 2.600000e-01)
-        self.interp_A = interp1d(x, y, kind='cubic')
+        self.interp_A = interp1d(x, y, kind='cubic', bounds_error=False, fill_value=(y[0], y[-1]))
         y = (1.466904e+00, 1.521782e+00, 1.559186e+00, 1.614585e+00, 1.869936e+00, 2.128056e+00, 2.301275e+00, 2.529241e+00, 2.661983e+00)
-        self.interp_a = interp1d(x, y, kind='cubic')
+        self.interp_a = interp1d(x, y, kind='cubic', bounds_error=False, fill_value=(y[0], y[-1]))
         y = (2.571104e+00, 2.254217e+00, 2.048674e+00, 1.869559e+00, 1.588649e+00, 1.507134e+00, 1.464374e+00, 1.436827e+00, 1.405210e+00)
-        self.interp_b = interp1d(x, y, kind='cubic')
+        self.interp_b = interp1d(x, y, kind='cubic', bounds_error=False, fill_value=(y[0], y[-1]))
         y = (1.193958e+00, 1.270316e+00, 1.335191e+00, 1.446266e+00, 1.581345e+00, 1.795050e+00, 1.965613e+00, 2.237466e+00, 2.439729e+00)
-        self.interp_c = interp1d(x, y, kind='cubic')
+        self.interp_c = interp1d(x, y, kind='cubic', bounds_error=False, fill_value=(y[0], y[-1]))
 
     def compute_HMF(self, cosmology, z, k, Pk):
         """Compute Tinker HMF and apply redshift volume."""
@@ -63,13 +63,8 @@ class HMFCalculator:
         """For given redshift and mean overdensity, return list of four Tinker
         parameters. If outside defined overdensity, return last valid number."""
         # Parameters at z=0
-        if Deltamean<200:
-            A, a, b, c = [.186, 1.47, 2.57, 1.19]
-        elif Deltamean>3200:
-            A, a, b, c = [.26, 2.66, 1.41, 2.44]
-        else:
-            lnDeltamean = np.log(Deltamean)
-            A, a, b, c = [self.interp_A(lnDeltamean), self.interp_a(lnDeltamean), self.interp_b(lnDeltamean), self.interp_c(lnDeltamean)]
+        lnDeltamean = np.log(Deltamean)
+        A, a, b, c = [self.interp_A(lnDeltamean), self.interp_a(lnDeltamean), self.interp_b(lnDeltamean), self.interp_c(lnDeltamean)]
         # Redshift evolution
         logalpha = -(.75/np.log10(Deltamean/75.))**1.2
         alpha = 10**logalpha
