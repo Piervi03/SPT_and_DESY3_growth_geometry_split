c737542a6416d3a18e6d997f20101d73c47f97cb
Sebastian Bocquet <sebastian.bocquet@gmail.com>
2025-07-31 13:54:12 +0200

    normalized P(richness|xi)

--- P_richness_given_SZ.py
+++ P_richness_given_SZ.py
@@ -1,6 +1,6 @@
 import numpy as np
 from multiprocessing import Pool
-from scipy.interpolate import make_interp_spline
+from scipy.interpolate import interp1d
 from scipy.special import ndtr
 
 import scaling_relations
@@ -13,10 +13,9 @@ def lnlike(catalog, SPT_survey_tab, HMF, cosmology, scaling, surveyCutRichness, 
     """Returns ln-likelihood of all `catalog['richness']` measurements given
      `catalog['XI']`."""
     # Richness-mass relation, valid for all SPT fields
-    lnrichness_m = scaling_relations.lnmass2lnobs('richness',
-                                                  HMF['lnM_arr'][None, :], HMF['z_arr'][:, None],
-                                                  scaling, cosmology)
-    lnrichness_m_interp = make_interp_spline(HMF['z_arr'], lnrichness_m, axis=0, k=1)
+    lnrichness_z_m = scaling_relations.lnmass2lnobs('richness',
+                                                    HMF['lnM_arr'][None, :], HMF['z_arr'][:, None],
+                                                    scaling, cosmology)
     # Cycle through SPT fields (because that affects the zeta-mass relation)
     field_idx = np.nonzero([SPT_survey_tab['LAMBDA_MIN'][i] in ['deep', 'shallow']
                             for i in range(len(SPT_survey_tab))])[0]
@@ -25,7 +24,7 @@ def lnlike(catalog, SPT_survey_tab, HMF, cosmology, scaling, surveyCutRichness, 
                                                catalog['SPT_ID', 'XI', 'richness', 'REDSHIFT', 'FIELD'],
                                                surveyCutRichness, richness_scatter_model,
                                                HMF['z_arr'], HMF['lnM_arr'], HMF['richness_SZ_lndNdlnM'],
-                                               lnrichness_m_interp,
+                                               lnrichness_z_m,
                                                scaling, cosmology)
                                  for i in field_idx])
     else:
@@ -44,35 +43,44 @@ def lnlike(catalog, SPT_survey_tab, HMF, cosmology, scaling, surveyCutRichness, 
 
 def process_field(SPT_field, catalog, surveyCutRichness, richness_scatter_model,
                   z_arr, lnM_arr, lndN_dz_dlnrichness_dlnzeta,
-                  lnrichness_m_interp,
+                  lnrichness_z_m,
                   scaling, cosmology):
     """Returns ln-likelihood of `catalog['richness']` given `catalog['XI']` for
     all clusters in `SPT_field`."""
+    lnzeta_z_m = scaling_relations.lnmass2lnobs('zeta', lnM_arr[None, :], z_arr[:, None],
+                                                scaling, cosmology,
+                                                SPTfield=SPT_field)
+    xi_z_m = scaling_relations.zeta2xi(np.exp(lnzeta_z_m))
     lambda_min = surveyCutRichness[SPT_field['LAMBDA_MIN']]
     lnlike = 0.
     field_idx = ((catalog['FIELD'] == SPT_field['FIELD']) & (catalog['richness'] > 0.)).nonzero()[0]
     for clusterID in field_idx:
-        # Look up dN/dlnlambda/dlnzeta at cluster redshift
-        lndN_dlnrichness_dlnzeta = (lndN_dz_dlnrichness_dlnzeta[np.argmin(np.abs(z_arr - catalog['REDSHIFT'][clusterID])), :, :]
+        # Look up dN/dlnlambda/dlnzeta and observables at cluster redshift
+        z_idx = np.argmin(np.abs(z_arr - catalog['REDSHIFT'][clusterID]))
+        lndN_dlnrichness_dlnzeta = (lndN_dz_dlnrichness_dlnzeta[z_idx, :, :]
                                     + np.log(scaling_relations.dlnM_dlnobs('richness', scaling, z=catalog['REDSHIFT'][clusterID])
                                              * scaling_relations.dlnM_dlnobs('zeta', scaling)))
-        # zeta and xi
-        lnzeta = scaling_relations.lnmass2lnobs('zeta', lnM_arr, catalog['REDSHIFT'][clusterID],
-                                                scaling, cosmology,
-                                                SPTfield=SPT_field)
-        xi = scaling_relations.zeta2xi(np.exp(lnzeta))
-        # Integrate over xi from max(xi-5, xi_min) to xi+3 to get (dN/dlnrichness)|xi_obs
+        lnrichness = lnrichness_z_m[z_idx, :]
+        lnzeta = lnzeta_z_m[z_idx, :]
+        xi = xi_z_m[z_idx, :]
+        # Interpolate to fine xi array w/ Delta xi = 0.25 from max(xi-5, xi_min) to xi+3
         xi_min = np.amax([scaling_relations.zeta2xi(scaling['zeta_min']), catalog['XI'][clusterID] - 5.])
-        idx = (xi > xi_min) & (xi < catalog['XI'][clusterID] + 3.)
-        lnP_xi = -.5 * (catalog['XI'][clusterID] - xi[idx])**2.
-        lnitg = lnP_xi + lndN_dlnrichness_dlnzeta[:, idx]
+        xi_arr = np.arange(xi_min, catalog['XI'][clusterID] + 3., .25)
+        lnzeta_arr = np.log(scaling_relations.xi2zeta(xi_arr))
+        with np.errstate(invalid='ignore'):
+            lndN_dlnrichness_dlnzeta_arr = interp1d(lnzeta, lndN_dlnrichness_dlnzeta, axis=1, assume_sorted=True)(lnzeta_arr)
+        lndN_dlnrichness_dlnzeta_arr[np.isnan(lndN_dlnrichness_dlnzeta_arr)] = -np.inf
+        # Condition on measured xi to get dN/dlnrichness
+        lnP_xi = -.5 * (catalog['XI'][clusterID] - xi_arr)**2.
+        lnitg = lnP_xi + lndN_dlnrichness_dlnzeta_arr
         with np.errstate(divide='ignore'):
             lndN_dlnrichness = np.log(np.sum(np.exp(.5 * (lnitg[:, :-1] + lnitg[:, 1:]))
-                                      * (lnzeta[idx][1:] - lnzeta[idx][:-1]), axis=1))
-        # Richness(z, M)
-        lnrichness = lnrichness_m_interp(catalog['REDSHIFT'][clusterID])
-        richness = np.exp(lnrichness)
+                                      * (lnzeta_arr[1:] - lnzeta_arr[:-1]), axis=1))
+        # Normalize to get dP/dlnrichness
+        norm = np.sum(np.exp(.5*(lndN_dlnrichness[:-1] + lndN_dlnrichness[1:])) * (lnrichness[1:] - lnrichness[:-1]))
+        lndN_dlnrichness -= np.log(norm)
         # P(richness_obs | richness), accounting for lambda_min
+        richness = np.exp(lnrichness)
         if richness_scatter_model == 'lognormalrelPoisson':
             # var(ln richness) = 1/richness
             lnrichnessobs = np.log(catalog['richness'][clusterID])
@@ -85,6 +93,7 @@ def process_field(SPT_field, catalog, surveyCutRichness, richness_scatter_model,
             with np.errstate(divide='ignore'):
                 lndP_dobs -= np.log(1. - ndtr((lambda_min(catalog['REDSHIFT'][clusterID])-richness)/np.sqrt(richness)))
         lndP_dobs[np.isposinf(lndP_dobs)] = -np.inf
+        lndP_dlnobs = lndP_dobs + lnrichness
         # ln-likelihood
         with np.errstate(invalid='ignore'):
             lnitg = lndP_dobs + lndN_dlnrichness
