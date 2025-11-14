5e60b336be2c7b5ff0dd5f8e61e176a1737130dc
Sebastian Bocquet <sebastian.bocquet@gmail.com>
2025-11-14 09:27:40 +0100

    option to store error on mean model

--- stacked_mass.py
+++ stacked_mass.py
@@ -8,7 +8,8 @@ def execute(HMF,
             SPT_survey_tab,
             survey_cut_lambda, richness_scatter_model,
             z_bins, rot_mat, rot_bins_x, rot_bins_y,
-            N_draws=1000000):
+            N_draws=1000000,
+            N_bootstrap=0):
     # Draws of all observables from the halo mass function
     z, xi, SNR, richness, lnMwl, lnw = P_Mwl_given_SZrichness.execute(HMF,
                                                                       cosmology, scaling,
@@ -23,6 +24,8 @@ def execute(HMF,
     rot_obs = np.matmul(rot_mat, [np.log(SNR), np.log(richness)]).T
     # Mean in bins
     Mwl_mean = np.zeros((len(z_bins)-1, len(rot_bins_x)-1, len(rot_bins_y)-1))
+    if N_bootstrap > 0:
+        Mwl_err = np.zeros_like(Mwl_mean)
     for i in range(len(z_bins)-1):
         i_idx = ((z >= z_bins[i]) & (z < z_bins[i+1])).nonzero()[0]
         for j in range(len(rot_bins_x)-1):
@@ -32,4 +35,13 @@ def execute(HMF,
                 if not np.any(ijk_idx):
                     continue
                 Mwl_mean[i, j, k] = np.sum(np.exp(lnMwl[ijk_idx]) * np.exp(lnw[ijk_idx])) / np.sum(np.exp(lnw[ijk_idx]))
-    return Mwl_mean
+                if N_bootstrap > 0:
+                    Mwl_boot = np.zeros(N_bootstrap)
+                    for b in range(N_bootstrap):
+                        boot_idx = np.random.choice(ijk_idx, size=len(ijk_idx), replace=True)
+                        Mwl_boot[b] = np.sum(np.exp(lnMwl[boot_idx]) * np.exp(lnw[boot_idx])) / np.sum(np.exp(lnw[boot_idx]))
+                    Mwl_err[i, j, k] = np.std(Mwl_boot)
+    if N_bootstrap > 0:
+        return Mwl_mean, Mwl_err
+    else:
+        return Mwl_mean
